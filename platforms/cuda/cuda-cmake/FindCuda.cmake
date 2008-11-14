f8a19f1ebf9a3a90a126334bc5857c84a5d281d0
Christopher Bruns <cmbruns@gmail.com>
2008-11-14 22:16:19 +0000

    Changed cmake to choose install directory the same way as in SimTKcommon project

--- platforms/cuda/cuda-cmake/FindCuda.cmake
+++ platforms/cuda/cuda-cmake/FindCuda.cmake
@@ -104,7 +104,7 @@ INCLUDE(${FINDCUDA_DIR}/CudaDependency.cmake)
 
 # Parse CUDA build type.
 IF (NOT CUDA_BUILD_TYPE)
-  SET(CUDA_BUILD_TYPE "Emulation" CACHE STRING "Cuda build type: Emulation or Device")
+  SET(CUDA_BUILD_TYPE "Device" CACHE STRING "Cuda build type: Emulation or Device")
 ENDIF(NOT CUDA_BUILD_TYPE)
 
 # Emulation if the card isn't present.
