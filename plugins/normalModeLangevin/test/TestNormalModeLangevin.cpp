98111cdd2ba5c65bdcc0fa4fc784f77bb4c78dc2
Christopher Bruns <cmbruns@gmail.com>
2009-12-01 00:37:57 +0000

    Avoid link error with TestNormalModeLangevin by linking to ${SHARED_TARGET} instead of to "OpenMM"

--- plugins/normalModeLangevin/test/TestNormalModeLangevin.cpp
+++ plugins/normalModeLangevin/test/TestNormalModeLangevin.cpp
@@ -2,23 +2,43 @@
 #include <vector>
 #include <string>
 #include <iostream>
+#include <cstdlib>
 
 using namespace OpenMM;
 using namespace std;
 
+void mysetenv(const char* name, const char* value) {
+#ifdef _MSC_VER
+    char buffer[1000];
+    sprintf(buffer, "%s=%s", name, value);
+    putenv(buffer);
+#else
+    setenv(name, value, 1);
+#endif
+}
+
 void testLoadNMLPlugin() 
 {
-    Platform::loadPluginsFromDirectory(Platform::getDefaultPluginsDirectory());
+    string pluginDir = Platform::getDefaultPluginsDirectory();
+    cout << "Default plugins directory = " << pluginDir << endl;
+    Platform::loadPluginsFromDirectory(pluginDir);
     vector<string> kernelName;
     kernelName.push_back("IntegrateNMLStepKernel");
     // Was NormalModeLangevin plugin loaded?
     Platform& platform = Platform::findPlatform(kernelName); // throws if no platform with kernel
 }
 
-int main() 
+int main(int argc, const char* argv[]) 
 {
     try 
     {
+        if (argc > 1) {
+            const char* plugin_dir = argv[1];
+            // 0 => don't set if variable exists
+            mysetenv("OPENMM_PLUGIN_DIR", plugin_dir);
+            cout << plugin_dir << endl;
+            cout << getenv("OPENMM_PLUGIN_DIR") << endl;
+        }
         testLoadNMLPlugin();
         cout << "tests passed" << endl;
         return 0;
