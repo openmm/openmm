6ccf6169841123be475d198277bba73ed8ecc7c8
Peter Eastman <peter.eastman@gmail.com>
2010-01-12 19:34:50 +0000

    Removed Brook platform

--- platforms/brook/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/friedrim/src/openmm/trunk/OpenMM/platforms/brook/tests
-# Build directory: /home/friedrim/src/openmm/trunk/OpenMM/platforms/brook/tests
-# 
-# This file replicates the SUBDIRS() and ADD_TEST() commands from the source
-# tree CMakeLists.txt file, skipping any SUBDIRS() or ADD_TEST() commands
-# that are excluded by CMake control structures, i.e. IF() commands.
-ADD_TEST(TestBrookStreamStatic "/home/friedrim/src/openmm/trunk/OpenMM/TestBrookStreamStatic")
