83889f1e6075a4e19bdcf434d40b88130dc1c992
Christopher Bruns <cmbruns@gmail.com>
2010-02-19 01:32:54 +0000

    Added some cmake to avoid running CUDA tests on non-gpu machines. So far tested only on GPU-having windows machine.

---
+++ platforms/cuda/tests/has_cuda_gpu.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <cuda_runtime.h>
+
+int main() {
+    int deviceCount, device;
+    int gpuDeviceCount = 0;
+    struct cudaDeviceProp properties;
+    cudaError_t cudaResultCode = cudaGetDeviceCount(&deviceCount);
+    if (cudaResultCode != cudaSuccess) 
+        deviceCount = 0;
+    for (device = 0; device < deviceCount; ++device) {
+        cudaGetDeviceProperties(&properties, device);
+        if (properties.major != 9999) /* 9999 means emulation only */
+            ++gpuDeviceCount;
+    }
+    printf("%d GPU CUDA device(s) found\n", gpuDeviceCount);
+
+    return gpuDeviceCount;
+}
+
