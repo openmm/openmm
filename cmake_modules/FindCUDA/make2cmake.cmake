68c89df6507f9df36c9a3488d86c2f67eed79243
Christopher Bruns <cmbruns@gmail.com>
2010-04-14 18:07:23 +0000

    Refactored cmake build system to use just one FindCUDA.cmake, under cmake_modules subdirectory, based on the FindCUDA.cmake that was in the platforms/cuda/cuda-make directory. Setting of CUDA_NVCC_FLAGS is now in top-level CMakeLists.txt, in case I ever figure out how to get this working with the cmake 2.8 installed system version of FindCUDA.cmake. Before this change, there were three separate FindCUDA.cmake areas.  Now there is just one.

--- cmake_modules/FindCUDA/make2cmake.cmake
+++ cmake_modules/FindCUDA/make2cmake.cmake
@@ -1,79 +1,65 @@
-#  James Bigler, NVIDIA Corp (nvidia.com - jbigler)
-#  Abe Stephens, SCI Institute -- http://www.sci.utah.edu/~abe/FindCuda.html
+
+#  For more information, please see: http://software.sci.utah.edu
 #
-#  Copyright (c) 2008 - 2009 NVIDIA Corporation.  All rights reserved.
+#  The MIT License
 #
-#  Copyright (c) 2007-2009
+#  Copyright (c) 2007
 #  Scientific Computing and Imaging Institute, University of Utah
 #
-#  This code is licensed under the MIT License.  See the FindCUDA.cmake script
-#  for the text of the license.
-
-# The MIT License
+#  License for the specific language governing rights and limitations under
+#  Permission is hereby granted, free of charge, to any person obtaining a
+#  copy of this software and associated documentation files (the "Software"),
+#  to deal in the Software without restriction, including without limitation
+#  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+#  and/or sell copies of the Software, and to permit persons to whom the
+#  Software is furnished to do so, subject to the following conditions:
 #
-# License for the specific language governing rights and limitations under
-# Permission is hereby granted, free of charge, to any person obtaining a
-# copy of this software and associated documentation files (the "Software"),
-# to deal in the Software without restriction, including without limitation
-# the rights to use, copy, modify, merge, publish, distribute, sublicense,
-# and/or sell copies of the Software, and to permit persons to whom the
-# Software is furnished to do so, subject to the following conditions:
+#  The above copyright notice and this permission notice shall be included
+#  in all copies or substantial portions of the Software.
 #
-# The above copyright notice and this permission notice shall be included
-# in all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-# DEALINGS IN THE SOFTWARE.
-#
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+#  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+#  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+#  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+#  DEALINGS IN THE SOFTWARE.
 
-#######################################################################
-# This converts a file written in makefile syntax into one that can be included
-# by CMake.
+# Make2cmake CMake Script
+# Abe Stephens and James Bigler
+# (c) 2007 Scientific Computing and Imaging Institute, University of Utah
+# Note that the REGEX expressions may need to be tweaked for different dependency generators.
 
-file(READ ${input_file} depend_text)
+FILE(READ ${input_file} depend_text)
 
-if (${depend_text} MATCHES ".+")
+IF (${depend_text} MATCHES ".+")
 
-  # message("FOUND DEPENDS")
+  # MESSAGE("FOUND DEPENDS")
 
   # Remember, four backslashes is escaped to one backslash in the string.
-  string(REGEX REPLACE "\\\\ " " " depend_text ${depend_text})
-
+  STRING(REGEX REPLACE "\\\\ " " " depend_text ${depend_text})
+  
   # This works for the nvcc -M generated dependency files.
-  string(REGEX REPLACE "^.* : " "" depend_text ${depend_text})
-  string(REGEX REPLACE "[ \\\\]*\n" ";" depend_text ${depend_text})
+  STRING(REGEX REPLACE "^.* : " "" depend_text ${depend_text})
+  STRING(REGEX REPLACE "[ \\\\]*\n" ";" depend_text ${depend_text})
 
-  set(dependency_list "")
+  FOREACH(file ${depend_text})
 
-  foreach(file ${depend_text})
+    STRING(REGEX REPLACE "^ +" "" file ${file})
 
-    string(REGEX REPLACE "^ +" "" file ${file})
+    # IF (EXISTS ${file})
+	  #   MESSAGE("DEPEND = ${file}")    
+    # ELSE (EXISTS ${file})
+	  #   MESSAGE("ERROR = ${file}")
+    # ENDIF(EXISTS ${file})
+  
+    SET(cuda_nvcc_depend "${cuda_nvcc_depend} \"${file}\"\n")
+  
+  ENDFOREACH(file) 
 
-    if(NOT IS_DIRECTORY ${file})
-      # If softlinks start to matter, we should change this to REALPATH.  For now we need
-      # to flatten paths, because nvcc can generate stuff like /bin/../include instead of
-      # just /include.
-      get_filename_component(file_absolute "${file}" ABSOLUTE)
-      list(APPEND dependency_list "${file_absolute}")
-    endif(NOT IS_DIRECTORY ${file})
+ELSE(${depend_text} MATCHES ".+") 
+  # MESSAGE("FOUND NO DEPENDS")
+ENDIF(${depend_text} MATCHES ".+")
 
-  endforeach(file)
 
-else()
-  # message("FOUND NO DEPENDS")
-endif()
-
-# Remove the duplicate entries and sort them.
-list(REMOVE_DUPLICATES dependency_list)
-list(SORT dependency_list)
-
-foreach(file ${dependency_list})
-  set(cuda_nvcc_depend "${cuda_nvcc_depend} \"${file}\"\n")
-endforeach()
-
-file(WRITE ${output_file} "# Generated by: make2cmake.cmake\nSET(CUDA_NVCC_DEPEND\n ${cuda_nvcc_depend})\n\n")
+FILE(WRITE ${output_file} "# Generated by: make2cmake.cmake\nSET(CUDA_NVCC_DEPEND\n ${cuda_nvcc_depend})\n\n")
--- cmake_modules/FindCUDA/parse_cubin.cmake
+++ cmake_modules/FindCUDA/parse_cubin.cmake
@@ -1,112 +1,105 @@
-#  James Bigler, NVIDIA Corp (nvidia.com - jbigler)
-#  Abe Stephens, SCI Institute -- http://www.sci.utah.edu/~abe/FindCuda.html
+#  For more information, please see: http://software.sci.utah.edu
 #
-#  Copyright (c) 2008 - 2009 NVIDIA Corporation.  All rights reserved.
+#  The MIT License
 #
-#  Copyright (c) 2007-2009
+#  Copyright (c) 2007
 #  Scientific Computing and Imaging Institute, University of Utah
 #
-#  This code is licensed under the MIT License.  See the FindCUDA.cmake script
-#  for the text of the license.
-
-# The MIT License
+#  License for the specific language governing rights and limitations under
+#  Permission is hereby granted, free of charge, to any person obtaining a
+#  copy of this software and associated documentation files (the "Software"),
+#  to deal in the Software without restriction, including without limitation
+#  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+#  and/or sell copies of the Software, and to permit persons to whom the
+#  Software is furnished to do so, subject to the following conditions:
 #
-# License for the specific language governing rights and limitations under
-# Permission is hereby granted, free of charge, to any person obtaining a
-# copy of this software and associated documentation files (the "Software"),
-# to deal in the Software without restriction, including without limitation
-# the rights to use, copy, modify, merge, publish, distribute, sublicense,
-# and/or sell copies of the Software, and to permit persons to whom the
-# Software is furnished to do so, subject to the following conditions:
+#  The above copyright notice and this permission notice shall be included
+#  in all copies or substantial portions of the Software.
 #
-# The above copyright notice and this permission notice shall be included
-# in all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-# DEALINGS IN THE SOFTWARE.
-#
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+#  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+#  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+#  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+#  DEALINGS IN THE SOFTWARE.
 
-#######################################################################
-# Parses a .cubin file produced by nvcc and reports statistics about the file.
+# .cubin Parsing CMake Script
+# Abe Stephens
+# (c) 2007 Scientific Computing and Imaging Institute, University of Utah
 
+FILE(READ ${input_file} file_text)
 
-file(READ ${input_file} file_text)
-
-if (${file_text} MATCHES ".+")
+IF (${file_text} MATCHES ".+")
 
   # Remember, four backslashes is escaped to one backslash in the string.
-  string(REGEX REPLACE ";" "\\\\;" file_text ${file_text})
-  string(REGEX REPLACE "\ncode" ";code" file_text ${file_text})
+  STRING(REGEX REPLACE ";" "\\\\;" file_text ${file_text})
+  STRING(REGEX REPLACE "\ncode" ";code" file_text ${file_text})
+  
+  LIST(LENGTH file_text len)
 
-  list(LENGTH file_text len)
-
-  foreach(line ${file_text})
+  FOREACH(line ${file_text})
 
     # Only look at "code { }" blocks.
-    if(line MATCHES "^code")
-
+    IF(line MATCHES "^code")
+      
       # Break into individual lines.
-      string(REGEX REPLACE "\n" ";" line ${line})
+      STRING(REGEX REPLACE "\n" ";" line ${line})
 
-      foreach(entry ${line})
+      FOREACH(entry ${line})
 
         # Extract kernel names.
-        if (${entry} MATCHES "[^g]name = ([^ ]+)")
-          string(REGEX REPLACE ".* = ([^ ]+)" "\\1" entry ${entry})
+        IF (${entry} MATCHES "[^g]name = ([^ ]+)")
+          STRING(REGEX REPLACE ".* = ([^ ]+)" "\\1" entry ${entry})
 
           # Check to see if the kernel name starts with "_"
-          set(skip FALSE)
-          # if (${entry} MATCHES "^_")
+          SET(skip FALSE)
+          # IF (${entry} MATCHES "^_")
             # Skip the rest of this block.
-            # message("Skipping ${entry}")
-            # set(skip TRUE)
-          # else (${entry} MATCHES "^_")
-            message("Kernel:    ${entry}")
-          # endif (${entry} MATCHES "^_")
+            # MESSAGE("Skipping ${entry}")
+            # SET(skip TRUE)
+          # ELSE (${entry} MATCHES "^_")
+            MESSAGE("Kernel:    ${entry}")  
+          # ENDIF (${entry} MATCHES "^_")
 
-        endif(${entry} MATCHES "[^g]name = ([^ ]+)")
+        ENDIF(${entry} MATCHES "[^g]name = ([^ ]+)")
 
         # Skip the rest of the block if necessary
-        if(NOT skip)
+        IF(NOT skip)
 
           # Registers
-          if (${entry} MATCHES "reg([ ]+)=([ ]+)([^ ]+)")
-            string(REGEX REPLACE ".*([ ]+)=([ ]+)([^ ]+)" "\\3" entry ${entry})
-            message("Registers: ${entry}")
-          endif()
-
+          IF (${entry} MATCHES "reg = ([^ ]+)")
+            STRING(REGEX REPLACE ".* = ([^ ]+)" "\\1" entry ${entry})
+            MESSAGE("Registers: ${entry}")
+          ENDIF(${entry} MATCHES "reg = ([^ ]+)")
+          
           # Local memory
-          if (${entry} MATCHES "lmem([ ]+)=([ ]+)([^ ]+)")
-            string(REGEX REPLACE ".*([ ]+)=([ ]+)([^ ]+)" "\\3" entry ${entry})
-            message("Local:     ${entry}")
-          endif()
-
+          IF (${entry} MATCHES "lmem = ([^ ]+)")
+            STRING(REGEX REPLACE ".* = ([^ ]+)" "\\1" entry ${entry})
+            MESSAGE("Local:     ${entry}")
+          ENDIF(${entry} MATCHES "lmem = ([^ ]+)")
+          
           # Shared memory
-          if (${entry} MATCHES "smem([ ]+)=([ ]+)([^ ]+)")
-            string(REGEX REPLACE ".*([ ]+)=([ ]+)([^ ]+)" "\\3" entry ${entry})
-            message("Shared:    ${entry}")
-          endif()
+          IF (${entry} MATCHES "smem = ([^ ]+)")
+            STRING(REGEX REPLACE ".* = ([^ ]+)" "\\1" entry ${entry})
+            MESSAGE("Shared:    ${entry}")
+          ENDIF(${entry} MATCHES "smem = ([^ ]+)")
+                  
+          IF (${entry} MATCHES "^}")
+            MESSAGE("")
+          ENDIF(${entry} MATCHES "^}")
 
-          if (${entry} MATCHES "^}")
-            message("")
-          endif()
-
-        endif(NOT skip)
+        ENDIF(NOT skip)
 
 
-      endforeach(entry)
+      ENDFOREACH(entry)
 
-    endif(line MATCHES "^code")
+    ENDIF(line MATCHES "^code")
 
-  endforeach(line)
+  ENDFOREACH(line) 
 
-else()
-  # message("FOUND NO DEPENDS")
-endif()
+ELSE(${file_text} MATCHES ".+") 
+  # MESSAGE("FOUND NO DEPENDS")
+ENDIF(${file_text} MATCHES ".+")
 
 
