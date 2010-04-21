7f486d6b077aa00adef3b2e5b2f7f3668ba72d8a
Peter Eastman <peter.eastman@gmail.com>
2010-04-21 19:39:53 +0000

    Fixed compilation error under Windows

--- platforms/opencl/EncodeCLFiles.cmake
+++ platforms/opencl/EncodeCLFiles.cmake
@@ -6,8 +6,8 @@ FOREACH(file ${OPENCL_KERNELS})
     # Load the file contents and process it.
     FILE(STRINGS ${file} file_content NEWLINE_CONSUME)
     STRING(REPLACE \\\n \\\\\n file_content "${file_content}")
-    STRING(REPLACE \n \\n file_content "${file_content}")
     STRING(REPLACE \" \\\" file_content "${file_content}")
+    STRING(REPLACE \n \\n\"\n\" file_content "${file_content}")
 
     # Determine a name for the variable that will contain this file's contents
     FILE(RELATIVE_PATH filename ${CL_SOURCE_DIR}/kernels ${file})
