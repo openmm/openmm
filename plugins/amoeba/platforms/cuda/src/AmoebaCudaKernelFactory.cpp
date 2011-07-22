da4b14e12640c95deca1451be0529fe54612f1bb
Mark Friedrichs <friedrim@stanford.edu>
2011-07-22 21:34:15 +0000

    Turn off logging

--- plugins/amoeba/platforms/cuda/src/AmoebaCudaKernelFactory.cpp
+++ plugins/amoeba/platforms/cuda/src/AmoebaCudaKernelFactory.cpp
@@ -109,7 +109,7 @@ KernelImpl* AmoebaCudaKernelFactory::createKernelImpl(std::string name, const Pl
     if( mapIterator == contextToAmoebaDataMap.end() ){
         amoebaCudaData                         = new AmoebaCudaData( cudaPlatformData );
         contextToAmoebaDataMap[&context]       = amoebaCudaData;
-        amoebaCudaData->setLog( stderr );
+        //amoebaCudaData->setLog( stderr );
         amoebaCudaData->setContextImpl( static_cast<void*>(&context) );
     } else {
         amoebaCudaData                         = mapIterator->second;
