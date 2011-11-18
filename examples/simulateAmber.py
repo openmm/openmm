534fd40416011ebbbdf49deebecd32bcddaaffae
Peter Eastman <peter.eastman@gmail.com>
2011-11-18 22:41:40 +0000

    Added examples for app

---
+++ examples/simulateAmber.py
@@ -0,0 +1,13 @@
+from simtk.openmm.app import *
+from simtk.openmm import *
+from simtk.unit import *
+
+prmtop = AmberPrmtopFile('input.prmtop')
+inpcrd = AmberInpcrdFile('input.inpcrd')
+system = prmtop.createSystem(nonbondedMethod=PME, nonbondedCutoff=1*nanometer, constraints=HBonds)
+integrator = LangevinIntegrator(300*kelvin, 1/picosecond, 0.002*picoseconds)
+simulation = Simulation(prmtop.topology, system, integrator)
+simulation.context.setPositions(inpcrd.positions)
+simulation.minimizeEnergy()
+simulation.reporters.append(PDBReporter('output.pdb', 1000))
+simulation.step(100000)
