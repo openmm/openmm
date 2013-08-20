443dceb8533f66f2ce465bc35ca4351f2ea428cd
Robert McGibbon <rmcgibbo@gmail.com>
2013-08-19 19:28:03 -0700

    make testInstallation py2/3

--- examples/testInstallation.py
+++ examples/testInstallation.py
@@ -1,4 +1,6 @@
+from __future__ import print_function
 # First make sure OpenMM is installed.
+
 
 import sys
 try:
@@ -6,8 +8,8 @@ try:
     from simtk.openmm import *
     from simtk.unit import *
 except ImportError as err:
-    print "Failed to import OpenMM packages:", err.message
-    print "Make sure OpenMM is installed and the library path is set correctly."
+    print("Failed to import OpenMM packages:", err.message)
+    print("Make sure OpenMM is installed and the library path is set correctly.")
     sys.exit()
 
 # Create a System for the tests.
@@ -19,28 +21,28 @@ system = forcefield.createSystem(pdb.topology, nonbondedMethod=PME, nonbondedCut
 # List all installed platforms and compute forces with each one.
 
 numPlatforms = Platform.getNumPlatforms()
-print "There are", numPlatforms, "Platforms available:"
-print
+print("There are", numPlatforms, "Platforms available:")
+print()
 forces = [None]*numPlatforms
 for i in range(numPlatforms):
     platform = Platform.getPlatform(i)
-    print i+1, platform.getName(),
+    print(i+1, platform.getName(), end=" ")
     integrator = LangevinIntegrator(300*kelvin, 1/picosecond, 0.002*picoseconds)
     try:
         simulation = Simulation(pdb.topology, system, integrator, platform)
         simulation.context.setPositions(pdb.positions)
         forces[i] = simulation.context.getState(getForces=True).getForces()
         del simulation
-        print "- Successfully computed forces"
+        print("- Successfully computed forces")
     except:
-        print "- Error computing forces with", platform.getName(), "platform"
+        print("- Error computing forces with", platform.getName(), "platform")
 
 # See how well the platforms agree.
 
 if numPlatforms > 1:
-    print
-    print "Median difference in forces between platforms:"
-    print
+    print()
+    print("Median difference in forces between platforms:")
+    print()
     for i in range(numPlatforms):
         for j in range(i):
             if forces[i] is not None and forces[j] is not None:
@@ -49,4 +51,6 @@ if numPlatforms > 1:
                     d = f1-f2
                     error = sqrt((d[0]*d[0]+d[1]*d[1]+d[2]*d[2])/(f1[0]*f1[0]+f1[1]*f1[1]+f1[2]*f1[2]))
                     errors.append(error)
-                print "%s vs. %s: %g" % (Platform.getPlatform(j).getName(), Platform.getPlatform(i).getName(), sorted(errors)[len(errors)/2])
+                print("{} vs. {}: {:g}".format(Platform.getPlatform(j).getName(),
+                                              Platform.getPlatform(i).getName(),
+                                              sorted(errors)[len(errors)//2]))
