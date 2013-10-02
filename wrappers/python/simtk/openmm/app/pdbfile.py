25d8559fe12149b601bf77b905f9d07081cc4033
Robert McGibbon <rmcgibbo@gmail.com>
2013-10-01 22:41:25 -0700

    Fix to PR #152

--- wrappers/python/simtk/openmm/app/pdbfile.py
+++ wrappers/python/simtk/openmm/app/pdbfile.py
@@ -287,10 +287,14 @@ class PDBFile(object):
                     else:
                         atomName = atom.name
                     coords = positions[posIndex]
+                    if atom.element is not None:
+                        symbol = atom.element.symbol
+                    else:
+                        symbol = ' '
                     line = "ATOM  %5d %-4s %3s %s%4d    %s%s%s  1.00  0.00          %2s  " % (
                         atomIndex%100000, atomName, resName, chainName,
                         (resIndex+1)%10000, _format_83(coords[0]),
-                        _format_83(coords[1]), _format_83(coords[2]), atom.element.symbol)
+                        _format_83(coords[1]), _format_83(coords[2]), symbol)
                     assert len(line) == 80, 'Fixed width overflow detected'
                     print >>file, line
                     posIndex += 1
