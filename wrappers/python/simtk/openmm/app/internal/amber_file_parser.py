f7b98d0104260fad248d207d2a73aac203b318f7
Jason Swails <jason.swails@gmail.com>
2013-10-14 15:06:10 -0400

    Corrected minor bug in GBneck screening parameter assignment. The existing logic was:
    if symbl[0] == ('c' or 'C'):
       ...do stuff...
    
    This logic will match only 'c' as ('c' or 'C') evaluates to 'c' while
    ('c' and 'C') evaluates to 'C':
    
    >>> 'c' or 'C'
    'c'
    >>> 'C' == ('c' or 'C')
    False
    
    The correct logic is either
    
    if symbl[0].upper() == 'C':
       ...do stuff...
    
    or
    
    if symbl[0] in ('C', 'c'):
       ...do stuff...
    
    (Alternatives can be [ if symbl[0] in 'cC' ] or [ if symbl[0].lower() == 'c'])

--- wrappers/python/simtk/openmm/app/internal/amber_file_parser.py
+++ wrappers/python/simtk/openmm/app/internal/amber_file_parser.py
@@ -474,15 +474,15 @@ class PrmtopLoader(object):
         # Update screening parameters for GBn if specified
         if symbls:
             for (i, symbl) in enumerate(symbls):
-                if symbl[0] == ('c' or 'C'):
+                if symbl[0] in ('c', 'C'):
                     screen[i] = 0.48435382330
-                elif symbl[0] == ('h' or 'H'):
+                elif symbl[0] in ('h', 'H'):
                     screen[i] = 1.09085413633
-                elif symbl[0] == ('n' or 'N'):
+                elif symbl[0] in ('n', 'N'):
                     screen[i] = 0.700147318409
-                elif symbl[0] == ('o' or 'O'):
+                elif symbl[0] in ('o', 'O'):
                     screen[i] = 1.06557401132
-                elif symbl[0] == ('s' or 'S'):
+                elif symbl[0] in ('s', 'S'):
                     screen[i] = 0.602256336067
                 else:
                     screen[i] = 0.5
