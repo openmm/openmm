4be5daf13f212d9a663bc53a26db4e4a634f8c0b
peastman <peastman@stanford.edu>
2014-04-23 15:27:11 -0700

    Created LocalCoordinatesSite, including reference implementation

--- platforms/reference/tests/TestReferenceVirtualSites.cpp
+++ platforms/reference/tests/TestReferenceVirtualSites.cpp
@@ -6,7 +6,7 @@
  * Biological Structures at Stanford, funded under the NIH Roadmap for        *
  * Medical Research, grant U54 GM072970. See https://simtk.org.               *
  *                                                                            *
- * Portions copyright (c) 2012 Stanford University and the Authors.           *
+ * Portions copyright (c) 2012-2014 Stanford University and the Authors.      *
  * Authors: Peter Eastman                                                     *
  * Contributors:                                                              *
  *                                                                            *
@@ -216,6 +216,85 @@ void testOutOfPlane() {
 }
 
 /**
+ * Test a LocalCoordinatesSite virtual site.
+ */
+void testLocalCoordinates() {
+    const Vec3 originWeights(0.2, 0.3, 0.5);
+    const Vec3 xWeights(-1.0, 0.5, 0.5);
+    const Vec3 yWeights(0.0, -1.0, 1.0);
+    const Vec3 localPosition(0.4, 0.3, 0.2);
+    ReferencePlatform platform;
+    System system;
+    system.addParticle(1.0);
+    system.addParticle(1.0);
+    system.addParticle(1.0);
+    system.addParticle(0.0);
+    system.setVirtualSite(3, new LocalCoordinatesSite(0, 1, 2, originWeights, xWeights, yWeights, localPosition));
+    CustomExternalForce* forceField = new CustomExternalForce("2*x^2+3*y^2+4*z^2");
+    system.addForce(forceField);
+    vector<double> params;
+    forceField->addParticle(0, params);
+    forceField->addParticle(1, params);
+    forceField->addParticle(2, params);
+    forceField->addParticle(3, params);
+    LangevinIntegrator integrator(300.0, 0.1, 0.002);
+    Context context(system, integrator, platform);
+    vector<Vec3> positions(4), positions2(4), positions3(4);
+    OpenMM_SFMT::SFMT sfmt;
+    init_gen_rand(0, sfmt);
+    for (int i = 0; i < 100; i++) {
+        // Set the particles at random positions.
+        
+        Vec3 xdir, ydir, zdir;
+        do {
+            for (int j = 0; j < 3; j++)
+                positions[j] = Vec3(genrand_real2(sfmt), genrand_real2(sfmt), genrand_real2(sfmt));
+            xdir = positions[0]*xWeights[0] + positions[1]*xWeights[1] + positions[2]*xWeights[2];
+            ydir = positions[0]*yWeights[0] + positions[1]*yWeights[1] + positions[2]*yWeights[2];
+            zdir = xdir.cross(ydir);
+            if (sqrt(xdir.dot(xdir)) > 0.1 && sqrt(ydir.dot(ydir)) > 0.1 && sqrt(zdir.dot(zdir)) > 0.1)
+                break; // These positions give a reasonable coordinate system.
+        } while (true);
+        context.setPositions(positions);
+        context.applyConstraints(0.0001);
+        
+        // See if the virtual site is positioned correctly.
+        
+        State state = context.getState(State::Positions | State::Forces);
+        const vector<Vec3>& pos = state.getPositions();
+        Vec3 origin = pos[0]*originWeights[0] + pos[1]*originWeights[1] + pos[2]*originWeights[2];
+        xdir /= sqrt(xdir.dot(xdir));
+        zdir /= sqrt(zdir.dot(zdir));
+        ydir = zdir.cross(xdir);
+        ASSERT_EQUAL_VEC(origin+xdir*localPosition[0]+ydir*localPosition[1]+zdir*localPosition[2], pos[3], 1e-10);
+
+        // Take a small step in the direction of the energy gradient and see whether the potential energy changes by the expected amount.
+
+        double norm = 0.0;
+        for (int i = 0; i < 3; ++i) {
+            Vec3 f = state.getForces()[i];
+            norm += f[0]*f[0] + f[1]*f[1] + f[2]*f[2];
+        }
+        norm = std::sqrt(norm);
+        const double delta = 1e-2;
+        double step = 0.5*delta/norm;
+        for (int i = 0; i < 3; ++i) {
+            Vec3 p = positions[i];
+            Vec3 f = state.getForces()[i];
+            positions2[i] = Vec3(p[0]-f[0]*step, p[1]-f[1]*step, p[2]-f[2]*step);
+            positions3[i] = Vec3(p[0]+f[0]*step, p[1]+f[1]*step, p[2]+f[2]*step);
+        }
+        context.setPositions(positions2);
+        context.applyConstraints(0.0001);
+        State state2 = context.getState(State::Energy);
+        context.setPositions(positions3);
+        context.applyConstraints(0.0001);
+        State state3 = context.getState(State::Energy);
+        ASSERT_EQUAL_TOL(norm, (state2.getPotentialEnergy()-state3.getPotentialEnergy())/delta, 1e-3)
+    }
+}
+
+/**
  * Make sure that energy, linear momentum, and angular momentum are all conserved
  * when using virtual sites.
  */
@@ -281,6 +360,26 @@ void testConservationLaws() {
     positions.push_back(Vec3(2, 0, -1));
     positions.push_back(Vec3(1, 1, -1));
     positions.push_back(Vec3());
+    
+    // Create a molecule with a LocalCoordinatesSite virtual site.
+    
+    system.addParticle(1.0);
+    system.addParticle(1.0);
+    system.addParticle(1.0);
+    system.addParticle(0.0);
+    system.setVirtualSite(14, new LocalCoordinatesSite(11, 12, 13, Vec3(0.3, 0.3, 0.4), Vec3(1.0, -0.5, -0.5), Vec3(0, -1.0, 1.0), Vec3(0.2, 0.2, 1.0)));
+    system.addConstraint(11, 12, 1.0);
+    system.addConstraint(11, 13, 1.0);
+    system.addConstraint(12, 13, sqrt(2.0));
+    for (int i = 0; i < 4; i++) {
+        forceField->addParticle(0, 1, 10);
+        for (int j = 0; j < i; j++)
+            forceField->addException(i+11, j+11, 0, 1, 0);
+    }
+    positions.push_back(Vec3(1, 2, 0));
+    positions.push_back(Vec3(2, 2, 0));
+    positions.push_back(Vec3(1, 3, 0));
+    positions.push_back(Vec3());
 
     // Simulate it and check conservation laws.
     
@@ -329,6 +428,7 @@ int main() {
         testTwoParticleAverage();
         testThreeParticleAverage();
         testOutOfPlane();
+        testLocalCoordinates();
         testConservationLaws();
     }
     catch(const exception& e) {
