cb0ae089b7f9030c3e90c7e8e3cd6d1d45bc9f57
John Chodera (MSKCC) <choderaj@mskcc.org>
2014-07-04 01:26:12 -0400

    One more try at BINSTAR_TOKEN

--- tools/ci/after_sucess.sh
+++ tools/ci/after_sucess.sh
@@ -12,7 +12,6 @@ fi
 
 if [[ "2.7 3.3" =~ "$python" ]]; then
     conda install --yes binstar
-    echo binstar -t $BINSTAR_TOKEN upload --force -u omnia -p openmm-dev $HOME/miniconda/conda-bld/linux-64/openmm-dev-*
     binstar -t $BINSTAR_TOKEN upload --force -u omnia -p openmm-dev $HOME/miniconda/conda-bld/linux-64/openmm-dev-*
 fi
 
