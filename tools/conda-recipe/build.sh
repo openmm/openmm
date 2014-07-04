ec43cdcc88b61deab446a2adee2bd12f8bd380f2
John Chodera (MSKCC) <choderaj@mskcc.org>
2014-07-04 10:25:51 -0400

    Restored parallel conda build; attempting to add C tests.

--- tools/conda-recipe/build.sh
+++ tools/conda-recipe/build.sh
@@ -30,8 +30,7 @@ cp -r $RECIPE_DIR/../.. .
 mkdir build
 cd build
 cmake .. $CMAKE_FLAGS
-#make -j4 # faster
-make # simpler to debug
+make -j4
 make install
 
 export OPENMM_INCLUDE_PATH=$PREFIX/include
