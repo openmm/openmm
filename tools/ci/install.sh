01494ff9f052d5d3794a5df84aa94b0e5de446a6
John Chodera (MSKCC) <choderaj@mskcc.org>
2014-07-03 23:01:43 -0400

    Updated preinstall script with more prereqs.

--- tools/ci/install.sh
+++ tools/ci/install.sh
@@ -1,3 +1,10 @@
+# Original OpenMM .travis.yml requirements.
+sudo apt-get update -qq
+sudo apt-get install -qq libpcre3 libpcre3-dev gromacs
+sudo apt-get install -qq swig doxygen llvm-3.3
+export ASAN_SYMBOLIZER_PATH=/usr/bin/llvm-symbolizer-3.3
+
+# New requirements.
 sudo apt-get install -qq -y g++ gfortran csh
 sudo apt-get install -qq -y g++-multilib gcc-multilib
 wget http://repo.continuum.io/miniconda/Miniconda-3.0.5-Linux-x86_64.sh
@@ -10,4 +17,4 @@ conda update --yes conda
 conda config --add channels http://conda.binstar.org/omnia
 conda create --yes -n ${python} python=${python} --file tools/ci/requirements-conda.txt
 source activate $python
-$HOME/miniconda/envs/${python}/bin/pip install $PIP_ARGS pymbar==2.0.1-beta nose-exclude
+$HOME/miniconda/envs/${python}/bin/pip install $PIP_ARGS nose-exclude
