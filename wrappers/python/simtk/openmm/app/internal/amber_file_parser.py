a558b3b0abb5999f51876695beb13ce1d6ec9f46
Jason Swails <jason.swails@gmail.com>
2014-07-11 13:51:31 -0700

    Copyright update.

--- wrappers/python/simtk/openmm/app/internal/amber_file_parser.py
+++ wrappers/python/simtk/openmm/app/internal/amber_file_parser.py
@@ -12,7 +12,7 @@ Simbios, the NIH National Center for Physics-Based Simulation of
 Biological Structures at Stanford, funded under the NIH Roadmap for
 Medical Research, grant U54 GM072970. See https://simtk.org.
 
-Portions copyright (c) 2012-2013 Stanford University and the Authors.
+Portions copyright (c) 2012-2014 Stanford University and the Authors.
 Authors: Randall J. Radmer, John D. Chodera, Peter Eastman
 Contributors: Christoph Klein, Michael R. Shirts, Jason Swails
 
