6ddb633cf04b2da87ee35bf86d705d592db0b9ff
kyleabeauchamp <kyleabeauchamp@gmail.com>
2014-07-21 20:22:23 -0400

    Remove hardcoded reference to my fork in build_conda_package_vagrant.sh

--- devtools/build-vm/linux/build_conda_package_vagrant.sh
+++ devtools/build-vm/linux/build_conda_package_vagrant.sh
@@ -2,7 +2,7 @@ export PATH=$HOME/miniconda/bin:$PATH
 export SWIG_LIB=$HOME/miniconda/share/swig/
 export CC="clang++"
 
-git clone -b vagrant https://github.com/kyleabeauchamp/openmm.git
+git clone -b vagrant https://github.com/simtk/openmm.git
 cd openmm
 conda install --file tools/ci/requirements-conda.txt --yes
 conda build tools/conda-recipe
