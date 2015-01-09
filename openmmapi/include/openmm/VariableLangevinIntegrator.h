b705a22e84752ddd4c037010419cfbc25aef53d2
Jason Swails <jason.swails@gmail.com>
2015-01-09 06:36:21 -0500

    Add notes in the doxygen comments about setRandomNumberSeed resulting in a "truly" random seed being generated from the wall clock upon Context creation.

--- openmmapi/include/openmm/VariableLangevinIntegrator.h
+++ openmmapi/include/openmm/VariableLangevinIntegrator.h
@@ -121,6 +121,9 @@ public:
      * the other hand, no guarantees are made about the behavior of simulations that use the same seed.
      * In particular, Platforms are permitted to use non-deterministic algorithms which produce different
      * results on successive runs, even if those runs were initialized identically.
+     *
+     * If seed is set to 0 (which is the default value assigned), a new seed is generated from the system
+     * clock when a Context is created from this Force.
      */
     void setRandomNumberSeed(int seed) {
         randomNumberSeed = seed;
