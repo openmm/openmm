c6a12280c94eb33f4efd811d0530e3a2a58f3820
John Chodera (MSKCC) <choderaj@mskcc.org>
2015-02-18 16:40:10 +0100

    Updates to devtools/ci

--- devtools/ci/install.sh
+++ devtools/ci/install.sh
@@ -2,20 +2,20 @@
 sudo apt-get update -qq
 sudo apt-get install -qq libpcre3 libpcre3-dev gromacs
 sudo apt-get install -qq swig doxygen llvm-3.3
-sudo apt-get install -qq libgl1-mesa-dev opencl-headers fglrx=2:8.960-0ubuntu1 # for opencl support
+#sudo apt-get install -qq libgl1-mesa-dev opencl-headers fglrx=2:8.960-0ubuntu1 # for opencl support
 export ASAN_SYMBOLIZER_PATH=/usr/bin/llvm-symbolizer-3.3
 
 # New requirements.
 sudo apt-get install -qq -y g++ gfortran csh
 sudo apt-get install -qq -y g++-multilib gcc-multilib
-wget http://repo.continuum.io/miniconda/Miniconda-3.0.5-Linux-x86_64.sh
-bash Miniconda-3.0.5-Linux-x86_64.sh -b
+wget http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh
+bash Miniconda-latest-Linux-x86_64.sh -b
 PIP_ARGS="-U"
 
 export PATH=$HOME/miniconda/bin:$PATH
 
 conda update --yes conda
 conda config --add channels http://conda.binstar.org/omnia
-conda create --yes -n ${python} python=${python} --file tools/ci/requirements-conda.txt
+conda create --yes -n ${python} python=${python} --file devtools/ci/requirements-conda.txt
 source activate $python
 $HOME/miniconda/envs/${python}/bin/pip install $PIP_ARGS nose-exclude
