4543bca2deba926d9d4ec3e887dd98d8164aa473
John Chodera (MSKCC) <choderaj@mskcc.org>
2015-07-13 16:45:53 -0400

    Added scripts for source packaging.

---
+++ devtools/packaging/scripts/source/prepare.sh
@@ -0,0 +1,30 @@
+#!/bin/tcsh
+
+# Prepare for build by ensuring necessary prerequisites are locally installed.
+
+# Set relative workspace path.
+export WORKSPACE=`pwd`
+
+# Install miniconda
+export VERSION="Latest"
+export PLATFORM="Linux"
+export ARCH="x86_64"
+export MINICONDA="Miniconda-$VERSION-$PLATFORM-$ARCH.sh"
+if [ -f miniconda ];
+then
+   echo "miniconda already exists"
+else
+   echo "Downloading miniconda..."
+   rm -rf Miniconda-*
+   wget --quiet http://repo.continuum.io/miniconda/${MINICONDA}
+   bash ${MINICONDA} -b -p miniconda
+   PIP_ARGS="-U"
+fi
+
+# Add to path.
+export PATH=$WORKSPACE/miniconda/bin:$PATH
+
+# Ensure configuration is up to date.
+conda config --add channels http://conda.binstar.org/omnia
+conda install --yes --quiet swig fftw3f pip
+pip install sphinxcontrib-bibtex
