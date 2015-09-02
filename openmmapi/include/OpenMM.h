ca805216ce2b4f390611706aae96f46defaa0853
peastman <peastman@stanford.edu>
2015-09-02 16:53:16 -0700

    Added CustomCentroidBondForce to OpenMM.h

--- openmmapi/include/OpenMM.h
+++ openmmapi/include/OpenMM.h
@@ -9,7 +9,7 @@
  * Biological Structures at Stanford, funded under the NIH Roadmap for        *
  * Medical Research, grant U54 GM072970. See https://simtk.org.               *
  *                                                                            *
- * Portions copyright (c) 2009 Stanford University and the Authors.           *
+ * Portions copyright (c) 2009-2015 Stanford University and the Authors.      *
  * Authors: Peter Eastman                                                     *
  * Contributors:                                                              *
  *                                                                            *
@@ -37,6 +37,7 @@
 #include "openmm/CMAPTorsionForce.h"
 #include "openmm/CMMotionRemover.h"
 #include "openmm/CustomBondForce.h"
+#include "openmm/CustomCentroidBondForce.h"
 #include "openmm/CustomCompoundBondForce.h"
 #include "openmm/CustomAngleForce.h"
 #include "openmm/CustomTorsionForce.h"
