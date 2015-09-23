97f5e6112c10bda4c84eac22698213af69f4317e
Peter Eastman <peter.eastman@gmail.com>
2015-09-23 16:57:50 -0700

    Fixed compilation error on Windows

--- platforms/cpu/tests/CpuTests.h
+++ platforms/cpu/tests/CpuTests.h
@@ -29,6 +29,9 @@
  * USE OR OTHER DEALINGS IN THE SOFTWARE.                                     *
  * -------------------------------------------------------------------------- */
 
+#ifdef WIN32
+  #define _USE_MATH_DEFINES // Needed to get M_PI
+#endif
 #include "CpuPlatform.h"
 #include <cstdlib>
 #include <iostream>
