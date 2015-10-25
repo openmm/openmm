1bea63f528048f68ffc541ae2a385a87e696ed9e
Robert McGibbon <rmcgibbo@gmail.com>
2015-10-25 16:40:44 -0700

    Control what device is used for OpenCL tests

--- platforms/opencl/tests/OpenCLTests.h
+++ platforms/opencl/tests/OpenCLTests.h
@@ -40,4 +40,8 @@ OpenMM::OpenCLPlatform platform;
 void initializeTests(int argc, char* argv[]) {
     if (argc > 1)
         platform.setPropertyDefaultValue("OpenCLPrecision", std::string(argv[1]));
+    if (argc > 2)
+        platform.setPropertyDefaultValue("OpenCLPlatformIndex", std::string(argv[2]));
+    if (argc > 3)
+        platform.setPropertyDefaultValue("OpenCLDeviceIndex", std::string(argv[3]));
 }
