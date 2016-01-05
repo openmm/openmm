378cb3c4050df1c9a70baa6126aaa417c90c2f4a
Robert T. McGibbon <rmcgibbo@gmail.com>
2016-01-04 23:41:35 -0800

    Run through yapf

--- wrappers/python/setup.py
+++ wrappers/python/setup.py
@@ -1,6 +1,4 @@
-"""
-setup.py: Used for building python wrappers for Simbios' OpenMM library.
-"""
+from __future__ import print_function
 import ast
 import re
 import os
@@ -43,19 +41,15 @@ def libraryFilename(libName):
 
 
 def getLibraries():
-    libraries = ['OpenMM',
-                 'OpenMMAmoeba',
-                 'OpenMMRPMD',
-                 'OpenMMDrude']
+    libraries = ['OpenMM', 'OpenMMAmoeba', 'OpenMMRPMD', 'OpenMMDrude']
 
     if 'OPENMM_USE_DEBUG_LIBS' in os.environ:
         if platform.system() == "Windows":
             raise Exception("use of OpenMM debug libs not supported on Win OS")
         else:
-            sys.stdout.write("WARNING: using debug libs:\n")
-            for ii in range(len(libraries)):
-                libraries[ii] = "%s_d" % libraries[ii]
-                sys.stdout.write("%s\n" % libraries[ii])
+            print("WARNING: using debug libs")
+            libraries = ['%s_d' % l for l in libraries]
+            print(libraries)
     return libraries
 
 
@@ -63,7 +57,8 @@ class install_lib(_install_lib):
     def install(self):
         openMMLibPath = os.getenv('OPENMM_LIB_PATH')
         if not openMMLibPath:
-            reportError("Set OPENMM_LIB_PATH to point to the lib directory for OpenMM")
+            reportError(
+                "Set OPENMM_LIB_PATH to point to the lib directory for OpenMM")
 
         self.copyOpenMMLibraries(openMMLibPath)
         self.copyOpenMMPlugins(openMMLibPath)
@@ -87,8 +82,10 @@ class install_lib(_install_lib):
         shutil.copytree(src, dest)
 
 
-def writeVersionPy(filename="simtk/openmm/version.py", major_version_num=MAJOR_VERSION_NUM,
-                     minor_version_num=MINOR_VERSION_NUM, build_info=BUILD_INFO):
+def writeVersionPy(filename="simtk/openmm/version.py",
+                   major_version_num=MAJOR_VERSION_NUM,
+                   minor_version_num=MINOR_VERSION_NUM,
+                   build_info=BUILD_INFO):
     """Write a version.py file into the python source directory before installation.
     If a version.py file already exists, we assume that it contains only the git_revision
     information, since from within this python session in the python staging directory, we're
@@ -124,15 +121,16 @@ if not release:
     else:
         git_revision = 'Unknown'
 
-    version = full_version = '%s.%s.%s' % (major_version_num, minor_version_num, build_info)
+    version = full_version = '%s.%s.%s' % (major_version_num,
+                                           minor_version_num, build_info)
     if not IS_RELEASED:
         full_version += '.dev-' + git_revision[:7]
 
     a = open(filename, 'w')
     try:
         a.write(cnt % {'version': version,
-                       'full_version' : full_version,
-                       'git_revision' : git_revision,
+                       'full_version': full_version,
+                       'git_revision': git_revision,
                        'isrelease': str(IS_RELEASED)})
     finally:
         a.close()
@@ -142,33 +140,30 @@ def buildKeywordDictionary(major_version_num=MAJOR_VERSION_NUM,
                            minor_version_num=MINOR_VERSION_NUM,
                            build_info=BUILD_INFO):
 
-
     setupKeywords = {
         "name": "OpenMM",
-        "version": "%s.%s.%s" % (major_version_num, minor_version_num, build_info),
-        "author":  "Peter Eastman",
+        "version":
+        "%s.%s.%s" % (major_version_num, minor_version_num, build_info),
+        "author": "Peter Eastman",
         "license": "LGPL",
         "url": "https://simtk.org/home/openmm",
         "download_url": "https://simtk.org/home/openmm",
-        "packages": ["simtk",
-                     "simtk.unit",
-                     "simtk.openmm",
-                     "simtk.openmm.app",
+        "packages": ["simtk", "simtk.unit", "simtk.openmm", "simtk.openmm.app",
                      "simtk.openmm.app.internal",
                      "simtk.openmm.app.internal.charmm",
                      "simtk.openmm.app.internal.pdbx",
                      "simtk.openmm.app.internal.pdbx.reader",
                      "simtk.openmm.app.internal.pdbx.writer"],
         "data_files": [],
-        "package_data": {"simtk" : [],
-                         "simtk.unit" : [],
-                         "simtk.openmm" : [],
-                         "simtk.openmm.app" : ['data/*.xml', 'data/*.pdb'],
-                         "simtk.openmm.app.internal" : []},
+        "package_data": {"simtk": [],
+                         "simtk.unit": [],
+                         "simtk.openmm": [],
+                         "simtk.openmm.app": ['data/*.xml', 'data/*.pdb'],
+                         "simtk.openmm.app.internal": []},
         "platforms": ["Linux", "Mac OS X", "Windows"],
         "description": "High performance molecular simulation on GPUs",
         "long_description":
-            """OpenMM is a toolkit for molecular simulation. It can be used either as a
+        """OpenMM is a toolkit for molecular simulation. It can be used either as a
             stand-alone application for running simulations, or as a library you call
             from your own code. It provides a combination of extreme flexibility
             (through custom forces and integrators), openness, and high performance
@@ -180,21 +175,22 @@ def buildKeywordDictionary(major_version_num=MAJOR_VERSION_NUM,
     define_macros = [('MAJOR_VERSION', major_version_num),
                      ('MINOR_VERSION', minor_version_num)]
 
-
     openmm_include_path = os.getenv('OPENMM_INCLUDE_PATH')
     if not openmm_include_path:
-        reportError("Set OPENMM_INCLUDE_PATH to point to the include directory for OpenMM")
+        reportError(
+            "Set OPENMM_INCLUDE_PATH to point to the include directory for OpenMM")
 
-    extra_compile_args=[]
-    extra_link_args=[]
+    extra_compile_args = []
+    extra_link_args = []
     if platform.system() == "Windows":
-        define_macros.append( ('WIN32', None) )
-        define_macros.append( ('_WINDOWS', None) )
-        define_macros.append( (' _MSC_VER', None) )
+        define_macros.append(('WIN32', None))
+        define_macros.append(('_WINDOWS', None))
+        define_macros.append((' _MSC_VER', None))
         extra_compile_args.append('/EHsc')
     elif platform.system() == 'Darwin':
         extra_compile_args += ['-stdlib=libc++', '-mmacosx-version-min=10.7']
-        extra_link_args += ['-stdlib=libc++', '-mmacosx-version-min=10.7', '-Wl,-rpath,@loader_path/.']
+        extra_link_args += ['-stdlib=libc++', '-mmacosx-version-min=10.7',
+                            '-Wl,-rpath,@loader_path/.']
         # Hard-code CC and CXX to clang, since gcc/g++ will *not* work with
         # Anaconda, despite the fact that distutils will try to use them.
         # System Python, homebrew, and MacPorts on Macs will always use
@@ -203,7 +199,7 @@ def buildKeywordDictionary(major_version_num=MAJOR_VERSION_NUM,
         os.environ['CC'] = 'clang'
         os.environ['CXX'] = 'clang++'
     elif platform.system() == 'Linux':
-        extra_link_args = ['-Wl,-rpath,$ORIGIN/.',]
+        extra_link_args = ['-Wl,-rpath,$ORIGIN/.', ]
 
     libraries = getLibraries()
     library_dirs = [os.getenv('OPENMM_LIB_PATH')]
@@ -238,5 +234,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
