b6f5afa98fe26cac4846e87c31943f6e9e8da76b
Robert McGibbon <rmcgibbo@gmail.com>
2016-01-04 19:32:49 -0800

    Linux rpath

--- wrappers/python/setup.py
+++ wrappers/python/setup.py
@@ -187,27 +187,29 @@ def buildKeywordDictionary(major_version_num=MAJOR_VERSION_NUM,
         define_macros.append( ('_WINDOWS', None) )
         define_macros.append( (' _MSC_VER', None) )
         extra_compile_args.append('/EHsc')
-    else:
-        if platform.system() == 'Darwin':
-            extra_compile_args += ['-stdlib=libc++', '-mmacosx-version-min=10.7']
-            extra_link_args += ['-stdlib=libc++', '-mmacosx-version-min=10.7', '-Wl,-rpath,@loader_path/lib']
-            # Hard-code CC and CXX to clang, since gcc/g++ will *not* work with
-            # Anaconda, despite the fact that distutils will try to use them.
-            # System Python, homebrew, and MacPorts on Macs will always use
-            # clang, so this hack should always work and fix issues with users
-            # that have GCC installed from MacPorts or homebrew *and* Anaconda
-            os.environ['CC'] = 'clang'
-            os.environ['CXX'] = 'clang++'
+    elif platform.system() == 'Darwin':
+        extra_compile_args += ['-stdlib=libc++', '-mmacosx-version-min=10.7']
+        extra_link_args += ['-stdlib=libc++', '-mmacosx-version-min=10.7', '-Wl,-rpath,@loader_path/lib']
+        # Hard-code CC and CXX to clang, since gcc/g++ will *not* work with
+        # Anaconda, despite the fact that distutils will try to use them.
+        # System Python, homebrew, and MacPorts on Macs will always use
+        # clang, so this hack should always work and fix issues with users
+        # that have GCC installed from MacPorts or homebrew *and* Anaconda
+        os.environ['CC'] = 'clang'
+        os.environ['CXX'] = 'clang++'
+    elif platform.system() == 'Linux':
+        extra_link_args = ['-Wl,-rpath,$ORIGIN/lib',]
 
-    #library_dirs = [openmm_lib_path]
+    libraries = getLibraries()
+    library_dirs = [os.getenv('OPENMM_LIB_PATH')]
     include_dirs = openmm_include_path.split(';')
 
     extensionArgs = {"name": "simtk.openmm._openmm",
                      "sources": ["src/swig_doxygen/OpenMMSwig.cxx"],
                      "include_dirs": include_dirs,
                      "define_macros": define_macros,
-                     "libraries": getLibraries(),
-                     #"library"
+                     "libraries": libraries,
+                     "library_dirs": library_dirs,
                      "extra_compile_args": extra_compile_args,
                      "extra_link_args": extra_link_args}
 
