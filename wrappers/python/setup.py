9057aff7a416128ffb002425fec87d6753e243b5
Robert T. McGibbon <rmcgibbo@gmail.com>
2016-01-05 18:52:10 -0800

    This is looking good now

--- wrappers/python/setup.py
+++ wrappers/python/setup.py
@@ -1,28 +1,25 @@
 from __future__ import print_function
-import ast
 import re
 import os
 import sys
 import shutil
 import platform
 
-try:
+if 'bdist_wheel' in sys.argv:
     from setuptools import setup, Extension
-except ImportError:
+else:
     from distutils.core import setup, Extension
 
 from distutils.command.install_lib import install_lib as _install_lib
 
-# Configured by CMake when this filed is copied into the staging directory
+# Globals configured by CMake
 MAJOR_VERSION_NUM = '@OPENMM_MAJOR_VERSION@'
 MINOR_VERSION_NUM = '@OPENMM_MINOR_VERSION@'
 BUILD_INFO = '@OPENMM_BUILD_VERSION@'
-ALL_PLUGIN_TARGETS = '@ALL_PLUGIN_TARGETS@'
-WRAPPER_INCLUDE_DIRS = '@WRAPPER_INCLUDE_DIRS@'
-
-
-
-IS_RELEASED = False
+ALL_PLUGIN_TARGET_LOCATIONS = list(filter(None, '@ALL_PLUGIN_TARGET_LOCATIONS@'.split(';')))
+ALL_LIBRARIES = list(filter(None, '@ALL_LIBRARIES@'.split(';')))
+ALL_LIBRARY_LOCATIONS = list(filter(None, '@ALL_LIBRARY_LOCATIONS@'.split(';')))
+WRAPPER_INCLUDE_DIRS =  list(filter(None, '@WRAPPER_INCLUDE_DIRS@'.split(';')))
 
 __author__ = "Peter Eastman"
 __version__ = "%s.%s" % (MAJOR_VERSION_NUM, MINOR_VERSION_NUM)
@@ -35,111 +32,28 @@ def reportError(message):
     sys.exit(1)
 
 
-def libraryFilename(libName):
-    if platform.system() == 'Windows':
-        return '%s.dll' % libName
-    elif platform.system() == 'Darwin':
-        return 'lib%s.dylib' % libName
-    elif platform.system() == 'Linux':
-        return 'lib%s.so' % libName
-    else:
-        raise ValueError('Unknown platform: "%s"' % platform.system())
-
-
-def getLibraries():
-    libraries = ['OpenMM', 'OpenMMAmoeba', 'OpenMMRPMD', 'OpenMMDrude']
-
-    if 'OPENMM_USE_DEBUG_LIBS' in os.environ:
-        if platform.system() == "Windows":
-            raise Exception("use of OpenMM debug libs not supported on Win OS")
-        else:
-            print("WARNING: using debug libs")
-            libraries = ['%s_d' % l for l in libraries]
-            print(libraries)
-    return libraries
-
-
 class install_lib(_install_lib):
     def install(self):
-        openMMLibPath = os.getenv('OPENMM_LIB_PATH')
-        if not openMMLibPath:
-            reportError(
-                "Set OPENMM_LIB_PATH to point to the lib directory for OpenMM")
-
-        self.copyOpenMMLibraries(openMMLibPath)
-        self.copyOpenMMPlugins(openMMLibPath)
+        self.copyOpenMMLibraries()
+        self.copyOpenMMPlugins()
         return _install_lib.install(self)
 
-    def copyOpenMMLibraries(self, openMMLibPath):
-        dest = os.path.join(self.build_dir, 'simtk/openmm')
+    def copyOpenMMLibraries(self):
+        dest = os.path.join(self.build_dir, 'simtk/openmm/')
         if not os.path.exists(dest):
             os.makedirs(dest)
-        for libName in getLibraries():
-            libFileName = libraryFilename(libName)
-            src = os.path.join(openMMLibPath, libFileName)
+        for src in ALL_LIBRARY_LOCATIONS:
             shutil.copy2(src, dest)
 
-    def copyOpenMMPlugins(self, openMMLibPath):
-        dest = os.path.join(self.build_dir, 'simtk/openmm/plugins')
+    def copyOpenMMPlugins(self):
+        dest = os.path.join(self.build_dir, 'simtk/openmm/plugins/')
         if os.path.exists(dest):
             shutil.rmtree(dest)
+        if not os.path.exists(dest):
+            os.makedirs(dest)
 
-        src = os.path.join(openMMLibPath, 'plugins')
-        shutil.copytree(src, dest)
-
-
-def writeVersionPy(filename="simtk/openmm/version.py",
-                   major_version_num=MAJOR_VERSION_NUM,
-                   minor_version_num=MINOR_VERSION_NUM,
-                   build_info=BUILD_INFO):
-    """Write a version.py file into the python source directory before installation.
-    If a version.py file already exists, we assume that it contains only the git_revision
-    information, since from within this python session in the python staging directory, we're
-    not in the version controlled directory hierarchy.
-
-    When cmake is copying files into the PYTHON_STAGING_DIRECTORY, it will write the
-    git revision to version.py. We read that, and then overwrite it.
-    """
-
-    cnt = """
-# THIS FILE IS GENERATED FROM OPENMM SETUP.PY
-short_version = '%(version)s'
-version = '%(version)s'
-full_version = '%(full_version)s'
-git_revision = '%(git_revision)s'
-release = %(isrelease)s
-
-if not release:
-    version = full_version
-"""
-
-    if os.path.exists(filename):
-        # git_revision is written to the file by cmake
-        with open(filename) as f:
-            text = f.read()
-            match = re.search(r"git_revision\s+=\s+(.*)", text, re.MULTILINE)
-        try:
-            git_revision = ast.literal_eval(match.group(1))
-        except:
-            # except anything, including no re match or
-            # literal_eval failing
-            git_revision = 'Unknown'
-    else:
-        git_revision = 'Unknown'
-
-    version = full_version = '%s.%s.%s' % (major_version_num,
-                                           minor_version_num, build_info)
-    if not IS_RELEASED:
-        full_version += '.dev-' + git_revision[:7]
-
-    a = open(filename, 'w')
-    try:
-        a.write(cnt % {'version': version,
-                       'full_version': full_version,
-                       'git_revision': git_revision,
-                       'isrelease': str(IS_RELEASED)})
-    finally:
-        a.close()
+        for src in ALL_PLUGIN_TARGET_LOCATIONS:
+            shutil.copy2(src, dest)
 
 
 def buildKeywordDictionary(major_version_num=MAJOR_VERSION_NUM,
@@ -183,11 +97,6 @@ def buildKeywordDictionary(major_version_num=MAJOR_VERSION_NUM,
     define_macros = [('MAJOR_VERSION', major_version_num),
                      ('MINOR_VERSION', minor_version_num)]
 
-    openmm_include_path = os.getenv('OPENMM_INCLUDE_PATH')
-    if not openmm_include_path:
-        reportError(
-            "Set OPENMM_INCLUDE_PATH to point to the include directory for OpenMM")
-
     extra_compile_args = []
     extra_link_args = []
     if platform.system() == "Windows":
@@ -209,15 +118,13 @@ def buildKeywordDictionary(major_version_num=MAJOR_VERSION_NUM,
     elif platform.system() == 'Linux':
         extra_link_args = ['-Wl,-rpath,$ORIGIN/.', ]
 
-    libraries = getLibraries()
-    library_dirs = [os.getenv('OPENMM_LIB_PATH')]
-    include_dirs = openmm_include_path.split(';')
+    library_dirs = list(set([os.path.dirname(lib) for lib in ALL_LIBRARY_LOCATIONS]))
 
     extensionArgs = {"name": "simtk.openmm._openmm",
                      "sources": ["src/swig_doxygen/OpenMMSwig.cxx"],
-                     "include_dirs": include_dirs,
+                     "include_dirs": WRAPPER_INCLUDE_DIRS,
                      "define_macros": define_macros,
-                     "libraries": libraries,
+                     "libraries": ALL_LIBRARIES,
                      "library_dirs": library_dirs,
                      "extra_compile_args": extra_compile_args,
                      "extra_link_args": extra_link_args}
@@ -235,8 +142,6 @@ def main():
             reportError("OpenMM requires Mac OS X Leopard (10.5) or better.")
 
     setupKeywords = buildKeywordDictionary()
-    writeVersionPy()
-
     setup(**setupKeywords)
 
 
