72cec7eb581f6a71351787dbec27f5eac592d8fb
Saurabh Belsare <smbelsare@gmail.com>
2016-01-26 16:04:15 +0530

    Debugging 9

--- plugins/amoeba/platforms/reference/src/SimTKReference/AmoebaReferenceMultipoleForce.cpp
+++ plugins/amoeba/platforms/reference/src/SimTKReference/AmoebaReferenceMultipoleForce.cpp
@@ -1840,6 +1840,7 @@ void AmoebaReferenceMultipoleForce::calculateLabFramePermanentDipoles(const vect
            multipoleAtomCovalentInfo, particleData);
     cout << "This is a dipole test uniqueString number of particles" << _numParticles << endl;
     cout << "This is a dipole test uniqueString size of Particle Data" << particleData.size() << endl;
+    outputRotatedPermanentDipoles.resize(_numParticles);
     for (int i = 0; i < _numParticles; i++)
       {
       cout << "This is a dipole test uniqueString dipole moment i = " << i << " in x beforeAssign = " << particleData[i].dipole[0] << endl; 
