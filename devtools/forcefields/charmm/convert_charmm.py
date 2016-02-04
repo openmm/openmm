3c4e5e8593f5fc53cf588af0276150d436b9550e
ChayaSt <chaya.stern@choderalab.org>
2016-02-04 13:44:20 -0500

    updated convert_charmm.py

--- devtools/forcefields/charmm/convert_charmm.py
+++ devtools/forcefields/charmm/convert_charmm.py
@@ -1,13 +1,15 @@
 from parmed.charmm import CharmmParameterSet
 from parmed import openmm
 import glob
+import yaml
 
 # top and par files for Charmm36 and Charmm22
 charmm_files = glob.glob('charmm/toppar/*_all3*')
 charmm_files.extend(glob.glob('charmm/toppar/*_all2*'))
 
 # stream files
-stream_files = glob.glob('charmm/toppar/stream/prot/*.str')
+stream_files = glob.glob('charmm/toppar/*.str')
+stream_files.extend((glob.glob('charmm/toppar/stream/prot/*.str')))
 stream_files.extend(glob.glob('charmm/toppar/stream/carb/*.str'))
 stream_files.extend(glob.glob('charmm/toppar/stream/lipid/*.str'))
 stream_files.extend(glob.glob('charmm/toppar/stream/na/*.str'))
@@ -24,3 +26,6 @@ for file in stream_files:
     param = CharmmParameterSet(file)
     param_omm = openmm.OpenMMParameterSet.from_parameterset(param)
     param_omm.write('ffxml/%s.xml' % file[14:-4])
+
+# generate recommended combination for charmm36
+charmm36_files = yaml.load_all('charmm36.yaml')
