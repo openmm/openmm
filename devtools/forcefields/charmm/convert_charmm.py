c25743c68f9fa60f07aa86a8ebedaeaf0272f40b
ChayaSt <chaya.stern@choderalab.org>
2016-02-09 16:13:55 -0500

    updated convert_charmm.py to generate one large xml file

--- devtools/forcefields/charmm/convert_charmm.py
+++ devtools/forcefields/charmm/convert_charmm.py
@@ -5,27 +5,22 @@ import yaml
 
 # top and par files for Charmm36 and Charmm22
 charmm_files = glob.glob('charmm/toppar/*_all3*')
-charmm_files.extend(glob.glob('charmm/toppar/*_all2*'))
-
 # stream files
-stream_files = glob.glob('charmm/toppar/*.str')
-stream_files.extend((glob.glob('charmm/toppar/stream/prot/*.str')))
-stream_files.extend(glob.glob('charmm/toppar/stream/carb/*.str'))
-stream_files.extend(glob.glob('charmm/toppar/stream/lipid/*.str'))
-stream_files.extend(glob.glob('charmm/toppar/stream/na/*.str'))
-stream_files.extend(glob.glob('charmm/toppar/stream/misc/*.str'))
+charmm_files.extend(glob.glob('charmm/toppar/*.str'))
+# charmm_files.extend((glob.glob('charmm/toppar/stream/prot/*.str')))
+# charmm_files.extend(glob.glob('charmm/toppar/stream/carb/*.str'))
+# charmm_files.extend(glob.glob('charmm/toppar/stream/lipid/*.str'))
+# charmm_files.extend(glob.glob('charmm/toppar/stream/na/*.str'))
+# charmm_files.extend(glob.glob('charmm/toppar/stream/misc/*.str'))
 
 # convert top and par files to ffxml
-for file in charmm_files:
-    param = CharmmParameterSet(file)
-    param_omm = openmm.OpenMMParameterSet.from_parameterset(param)
-    param_omm.write('ffxml/%s.xml' % file[14:-4])
-
-# convert stream files to ffxml
-for file in stream_files:
-    param = CharmmParameterSet(file)
-    param_omm = openmm.OpenMMParameterSet.from_parameterset(param)
-    param_omm.write('ffxml/%s.xml' % file[14:-4])
+#for f in charmm_files:
+#    param = CharmmParameterSet(f)
+#    param_omm = openmm.OpenMMParameterSet.from_parameterset(param)
+#    param_omm.write('ffxml/%s.xml' % f[14:-4])
 
 # generate recommended combination for charmm36
-charmm36_files = yaml.load_all('charmm36.yaml')
+params = CharmmParameterSet(*charmm_files)
+#charmm_files.remove('charmm/toppar/stream/lipid/toppar_all36_lipid_list.str')
+params_omm = openmm.OpenMMParameterSet.from_parameterset(params)
+params_omm.write('ffxml/charmm36.xml')
