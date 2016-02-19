2c4f23a724ae1d1b21564f0e96c9d94c01b339f7
Rafal P. Wiewiora <rafwiewiora@gmail.com>
2016-02-19 13:04:26 -0500

    add dna and rna tests

--- devtools/forcefield-scripts/amber/conversion_script.py
+++ devtools/forcefield-scripts/amber/conversion_script.py
@@ -164,14 +164,38 @@ def validate_nucleic(ffxml_name, leaprc_name):
     dna_top = tempfile.mkstemp()
     dna_crd = tempfile.mkstemp()
     leap_script_dna_file = tempfile.mkstemp()
+    rna_top = tempfile.mkstemp()
+    rna_crd = tempfile.mkstemp()
+    leap_script_rna_file = tempfile.mkstemp()
+    leap_script_rna_file_alt = tempfile.mkstemp()
 
     print('Preparing LeaP scripts...')
-    leap_script_dna_string = """addPdbResMap {
+    leap_script_dna_string = """addPdbAtomMap {
+{ "H1'" "H1*" }
+{ "H2'" "H2'1" }
+{ "H2''" "H2'2" }
+{ "H3'" "H3*" }
+{ "H4'" "H4*" }
+{ "H5'" "H5'1" }
+{ "H5''" "H5'2" }
+{ "HO2'" "HO'2" }
+{ "HO5'" "H5T"  }
+{ "HO3'" "H3T" }
+{ "OP1" "O1P" }
+{ "OP2" "O2P" }
+}
+source %s
+addPdbResMap {
 { 0 "DG" "DG5"  } { 1 "DG" "DG3"  }
 { 0 "DA" "DA5"  } { 1 "DA" "DA3"  }
 { 0 "DC" "DC5"  } { 1 "DC" "DC3"  }
 { 0 "DT" "DT5"  } { 1 "DT" "DT3"  }
 }
+x = loadPdb files/4rzn_dna.pdb
+saveAmberParm x %s %s
+quit""" % (leaprc_name, dna_top[1], dna_crd[1])
+
+    leap_script_rna_string = """
 addPdbAtomMap {
 { "H1'" "H1*" }
 { "H2'" "H2'1" }
@@ -187,15 +211,56 @@ addPdbAtomMap {
 { "OP2" "O2P" }
 }
 source %s
-x = loadPdb files/4rzn_dna.pdb
+addPdbResMap {
+{ 0 "G" "G5"  } { 1 "G" "G3"  } { "G" "G" }
+{ 0 "A" "A5"  } { 1 "A" "A3"  } { "A" "A" }
+{ 0 "C" "C5"  } { 1 "C" "C3"  } { "C" "C" }
+{ 0 "U" "U5"  } { 1 "U" "U3"  } { "U" "U" }
+}
+x = loadPdb files/5c5w_rna.pdb
 saveAmberParm x %s %s
-quit""" % (leaprc_name, dna_top[1], dna_crd[1])
+quit""" % (leaprc_name, rna_top[1], rna_crd[1])
+
+    leap_script_rna_string_alt = """
+addPdbAtomMap {
+{ "H1'" "H1*" }
+{ "H2'" "H2'1" }
+{ "H2''" "H2'2" }
+{ "H3'" "H3*" }
+{ "H4'" "H4*" }
+{ "H5'" "H5'1" }
+{ "H5''" "H5'2" }
+{ "HO2'" "HO'2" }
+{ "HO5'" "H5T"  }
+{ "HO3'" "H3T" }
+{ "OP1" "O1P" }
+{ "OP2" "O2P" }
+}
+source %s
+addPdbResMap {
+{ 0 "G" "RG5"  } { 1 "G" "RG3"  } { "G" "RG" }
+{ 0 "A" "RA5"  } { 1 "A" "RA3"  } { "A" "RA" }
+{ 0 "C" "RC5"  } { 1 "C" "RC3"  } { "C" "RC" }
+{ 0 "U" "RU5"  } { 1 "U" "RU3"  } { "U" "RU" }
+}
+x = loadPdb files/5c5w_rna.pdb
+saveAmberParm x %s %s
+quit""" % (leaprc_name, rna_top[1], rna_crd[1])
+
     os.write(leap_script_dna_file[0], leap_script_dna_string)
+    os.write(leap_script_rna_file[0], leap_script_rna_string)
+    os.write(leap_script_rna_file_alt[0], leap_script_rna_string_alt)
 
     print('Running LEaP...')
     os.system('tleap -f %s' % leap_script_dna_file[1])
     if os.path.getsize(dna_top[1]) == 0 or os.path.getsize(dna_crd[1]) == 0:
         raise RuntimeError('DNA LEaP fail for %s' % leaprc_name)
+    os.system('tleap -f %s' % leap_script_rna_file[1])
+    if os.path.getsize(rna_top[1]) == 0 or os.path.getsize(rna_crd[1]) == 0:
+        # try alternative name mappings
+        os.system('tleap -f %s' % leap_script_rna_file_alt[1])
+    if os.path.getsize(rna_top[1]) == 0 or os.path.getsize(rna_crd[1]) == 0:
+        raise RuntimeError('RNA LEaP fail for %s' % leaprc_name)
 
     print('Calculating DNA energies...')
     # AMBER
@@ -209,8 +274,21 @@ quit""" % (leaprc_name, dna_top[1], dna_crd[1])
     system_omm = parm_omm.createSystem(splitDihedrals=True)
     dna_omm_energies = parmed.openmm.energy_decomposition_system(parm_omm, system_omm, nrg=kilojoules_per_mole)
 
+    print('Calculating RNA energies...')
+    # AMBER
+    parm_amber = parmed.load_file(rna_top[1], rna_crd[1])
+    system_amber = parm_amber.createSystem(splitDihedrals=True)
+    rna_amber_energies = parmed.openmm.energy_decomposition_system(parm_amber, system_amber, nrg=kilojoules_per_mole)
+    # OpenMM
+    ff = app.ForceField(ffxml_name)
+    system_omm = ff.createSystem(parm_amber.topology)
+    parm_omm = parmed.openmm.load_topology(parm_amber.topology, system_omm, xyz=parm_amber.positions)
+    system_omm = parm_omm.createSystem(splitDihedrals=True)
+    rna_omm_energies = parmed.openmm.energy_decomposition_system(parm_omm, system_omm, nrg=kilojoules_per_mole)
+
     print('Deleting temp files...')
-    for f in (dna_top, dna_crd, leap_script_dna_file):
+    for f in (dna_top, dna_crd, leap_script_dna_file, rna_top, rna_crd,
+              leap_script_rna_file, leap_script_rna_file_alt):
         os.unlink(f[1])
 
     print('Asserting DNA energies...')
@@ -225,6 +303,19 @@ quit""" % (leaprc_name, dna_top[1], dna_crd[1])
             err_msg=('DNA energies outside of allowed tolerance for %s' % ffxml_name))
             counter += 1
     print('DNA energy validation successful!')
+
+    print('Asserting RNA energies...')
+    counter = 0
+    for i, j in zip(rna_amber_energies, rna_omm_energies):
+        if counter != 3: # Not Impropers
+            testing.assert_allclose(j[1], i[1], rtol=1e-5,
+            err_msg=('RNA energies outside of allowed tolerance for %s' % ffxml_name))
+            counter += 1
+        else: # Impropers - higher tolerance
+            testing.assert_allclose(j[1], i[1], rtol=1e-2,
+            err_msg=('RNA energies outside of allowed tolerance for %s' % ffxml_name))
+            counter += 1
+    print('RNA energy validation successful!')
 
 def validate_gaff(ffxml_name, leaprc_name):
     if not find_executable('tleap'):
