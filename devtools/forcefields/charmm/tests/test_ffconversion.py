765c0bc292e30e201619bc7c11da926bb1d76d95
Chaya Stern <chaya.stern@choderalab.org>
2016-03-22 18:39:17 -0400

    added test

--- devtools/forcefields/charmm/tests/test_ffconversion.py
+++ devtools/forcefields/charmm/tests/test_ffconversion.py
@@ -6,7 +6,7 @@ import parmed.openmm as openmm
 
 platform = Platform.getPlatformByName('Reference')
 
-def compare_energies(ff, param, pdb, structure):
+def compare_energies_decomposition(ff, param, pdb, structure):
     system_omm = ff.createSystem(pdb.topology)
     system_parmed = structure.createSystem(param)
     structure.positions = pdb.positions
@@ -17,14 +17,39 @@ def compare_energies(ff, param, pdb, structure):
     print('pamed energies: %s' % parmed_energies)
     print('omm eneriges %s' % omm_energies)
 
-ff = ForceField('../ffxml/charmm36.xml')
-pdb = PDBFile('methanol_ions.pdb')
-structure = CharmmPsfFile('methanol_ions.psf')
-param = CharmmParameterSet('../charmm/toppar/top_all36_cgenff.rtf',
-                           '../charmm/toppar/par_all36_cgenff.prm',
-                           '../charmm/toppar/toppar_water_ions.str')
+def compare_energies(ff, param, pdb, structure, plat):
+    system_omm = ff.createSystem(pdb.topology)
+    system_parmed = structure.createSystem(param)
+    structure.positions = pdb.positions
+    structure_omm = openmm.load_topology(pdb.topology, system_omm, xyz=pdb.positions)
 
-compare_energies(ff, param, pdb, structure)
+    con_omm = Context(system_omm, VerletIntegrator(2*u.femtoseconds), plat)
+    con_omm.setPositions(pdb.positions)
+    state_omm = con_omm.getState(getEnergy=True)
+    ene_omm = state_omm.getPotentialEnergy().value_in_unit(u.kilocalories_per_mole)
+
+    con_charmm = Context(system_parmed, VerletIntegrator(2*u.femtoseconds), plat)
+    con_charmm.setPositions(pdb.positions)
+    state_charmm = con_charmm.getState(getEnergy=True)
+    ene_charmm = state_charmm.getPotentialEnergy().value_in_unit(u.kilocalories_per_mole)
+
+    print('omm total energy')
+    print(ene_omm)
+    print('charmm total energy')
+    print(ene_charmm)
+
+
+
+
+ff = ForceField('../ffxml/charmm36.xml')
+pdb = PDBFile('N_N_dimethylaniline.pdb')
+structure = CharmmPsfFile('N_N_dimethylaniline.psf')
+param = CharmmParameterSet('../toppar/top_all36_cgenff.rtf',
+                           '../toppar/par_all36_cgenff.prm',
+                           '../toppar/toppar_water_ions.str')
+
+compare_energies_decomposition(ff, param, pdb, structure)
+compare_energies(ff, param, pdb, structure, platform)
 
 # def test_ffconversion():
 #     psf = CharmmPsfFile('../ala_ala_ala.psf')
