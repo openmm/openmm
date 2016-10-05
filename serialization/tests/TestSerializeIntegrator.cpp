171f3a26a916fb2f3c942a88f8167fa3332dd3dd
Kyle Beauchamp <kyleabeauchamp@gmail.com>
2016-10-04 18:48:31 -0700

    Fix indent

--- serialization/tests/TestSerializeIntegrator.cpp
+++ serialization/tests/TestSerializeIntegrator.cpp
@@ -227,10 +227,10 @@ int main() {
         testSerializeLangevinIntegrator();
         testSerializeCompoundIntegrator();
     }
-catch(const exception& e) {
-    cout << "exception: " << e.what() << endl;
-    return 1;
-}
-cout << "Done" << endl;
-return 0;
+    catch(const exception& e) {
+        cout << "exception: " << e.what() << endl;
+        return 1;
+    }
+    cout << "Done" << endl;
+    return 0;
 }
