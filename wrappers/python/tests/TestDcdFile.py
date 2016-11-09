6129382916d4407d6c63cef653a1ae68d747bca9
peastman <peastman@stanford.edu>
2016-11-09 12:30:22 -0800

    Fixed error writing a DCD file for a very long trajectory

--- wrappers/python/tests/TestDcdFile.py
+++ wrappers/python/tests/TestDcdFile.py
@@ -17,6 +17,17 @@ class TestDCDFile(unittest.TestCase):
             for i in range(5):
                 dcd.writeModel([mm.Vec3(random(), random(), random()) for j in range(natom)]*unit.angstroms)
         os.remove(fname)
+    
+    def testLongTrajectory(self):
+        """Test writing a trajectory that has more than 2^31 steps."""
+        fname = tempfile.mktemp(suffix='.dcd')
+        pdbfile = app.PDBFile('systems/alanine-dipeptide-implicit.pdb')
+        natom = len(list(pdbfile.topology.atoms()))
+        with open(fname, 'wb') as f:
+            dcd = app.DCDFile(f, pdbfile.topology, 0.001, interval=1000000000)
+            for i in range(5):
+                dcd.writeModel([mm.Vec3(random(), random(), random()) for j in range(natom)]*unit.angstroms)
+        os.remove(fname)
 
 if __name__ == '__main__':
     unittest.main()
