4e6063d8924f0a0a229d9f51caa7260d71187dc1
peastman <peastman@stanford.edu>
2016-12-07 17:34:19 -0800

    Initial scripts for automating Windows builds

---
+++ devtools/packaging/scripts/windows/prepare.ps1
@@ -0,0 +1,20 @@
+cd C:\Users\vagrant
+
+# Install everything we can with choco.
+
+choco install -y doxygen.portable swig cmake doxygen.install vcbuildtools git jom
+
+# Install CUDA.
+
+wget https://developer.nvidia.com/compute/cuda/8.0/prod/network_installers/cuda_8.0.44_win10_network-exe -UseBasicParsing -OutFile cuda_8.0.44_win10_network.exe
+.\cuda_8.0.44_win10_network.exe -s compiler_8.0 cudart_8.0 cufft_8.0 cufft_dev_8.0 nvrtc_8.0 nvrtc_dev_8.0 | Out-Null
+
+# Install Miniconda.
+
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Windows-x86_64.exe -UseBasicParsing -OutFile Miniconda3-latest-Windows-x86_64.exe
+Start-Process -Wait -FilePath Miniconda3-latest-Windows-x86_64.exe -ArgumentList '/S'
+
+# Install software with conda.
+
+& "C:\Program Files\Miniconda3\Scripts\conda.exe" install -y -c omnia fftw3f jinja2 lxml sphinx sphinxcontrib-autodoc_doxygen sphinxcontrib-lunrsearch
+& "C:\Program Files\Miniconda3\Scripts\pip.exe" install sphinxcontrib.bibtex
