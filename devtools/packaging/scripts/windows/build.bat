03d8f66a5222b73cbab6e562842b41bae02dd705
peastman <peastman@stanford.edu>
2016-12-09 11:12:09 -0800

    Fixes to Windows build files

--- devtools/packaging/scripts/windows/build.bat
+++ devtools/packaging/scripts/windows/build.bat
@@ -1,16 +1,13 @@
-REM "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
-
-cd openmm
 mkdir build
 cd build
 
-set FFTW=C:\Program Files\Miniconda3\pkgs\fftw3f-3.3.4-vc14_2\Library
+set FFTW=C:\Miniconda3\pkgs\fftw3f-3.3.4-vc14_2\Library
 "C:\Program Files\CMake\bin\cmake.exe" .. -G "NMake Makefiles JOM" -DCMAKE_BUILD_TYPE=Release -DOPENMM_GENERATE_API_DOCS=ON -DFFTW_INCLUDES="%FFTW%/include" -DFFTW_LIBRARY="%FFTW%/lib/libfftw3f-3.lib"
 
 jom
 jom PythonInstall
 jom C++ApiDocs
 jom PythonApiDocs
-# jom sphinxpdf
+REM jom sphinxpdf
 jom install
 jom PythonBdist
--- devtools/packaging/scripts/windows/prepare.ps1
+++ devtools/packaging/scripts/windows/prepare.ps1
@@ -12,9 +12,9 @@ wget https://developer.nvidia.com/compute/cuda/8.0/prod/network_installers/cuda_
 # Install Miniconda.
 
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Windows-x86_64.exe -UseBasicParsing -OutFile Miniconda3-latest-Windows-x86_64.exe
-Start-Process -Wait -FilePath Miniconda3-latest-Windows-x86_64.exe -ArgumentList '/S'
+.\Miniconda3-latest-Windows-x86_64.exe /S /D=C:\Miniconda3 | Out-Null
 
 # Install software with conda.
 
-& "C:\Program Files\Miniconda3\Scripts\conda.exe" install -y -c omnia fftw3f jinja2 lxml sphinx sphinxcontrib-autodoc_doxygen sphinxcontrib-lunrsearch
-& "C:\Program Files\Miniconda3\Scripts\pip.exe" install sphinxcontrib.bibtex
+& "C:\Miniconda3\Scripts\conda.exe" install -y -c omnia fftw3f jinja2 lxml sphinx sphinxcontrib-autodoc_doxygen sphinxcontrib-lunrsearch conda-build
+& "C:\Miniconda3\Scripts\pip.exe" install sphinxcontrib.bibtex
