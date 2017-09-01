970cffa291ceb7aa2c4f516e4917c31efae846fe
peastman <peastman@stanford.edu>
2017-09-01 16:56:28 -0700

    Change to work correctly on volta

--- platforms/cuda/src/CudaContext.cpp
+++ platforms/cuda/src/CudaContext.cpp
@@ -240,7 +240,7 @@ CudaContext::CudaContext(const System& system, int deviceIndex, bool useBlocking
         // Don't generate Volta-specific code until we've made the changes needed
         // to support it properly.
         major = 6;
-        minor = 2;
+        minor = 0;
     }
     gpuArchitecture = intToString(major)+intToString(minor);
     computeCapability = major+0.1*minor;
