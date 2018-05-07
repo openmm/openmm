40b774c0b9b02f1260babca7a4393954b73aeadb
peastman <peastman@stanford.edu>
2018-05-07 10:59:21 -0700

    Removed 'fix' that isn't actually compatible with 7.2

--- wrappers/python/simtk/openmm/app/forcefield.py
+++ wrappers/python/simtk/openmm/app/forcefield.py
@@ -2439,9 +2439,6 @@ class LennardJonesGenerator(object):
             self.force.setNonbondedMethod(mm.CustomNonbondedForce.CutoffNonPeriodic)
         else:
             raise AssertionError('Unrecognized nonbonded method [%s]' % nonbondedMethod)
-        if args['switchDistance'] is not None:
-            self.force.setUseSwitchingFunction(True)
-            self.force.setSwitchingDistance(args['switchDistance'])
 
         # Add the particles
 
