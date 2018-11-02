2d0354d5a471c4dda24b2e726b9f3c2943b47f3d
Jason Swails <jason.swails@gmail.com>
2018-11-02 01:40:59 -0400

    Fix more build issues.

--- devtools/ci/jenkins/install.sh
+++ devtools/ci/jenkins/install.sh
@@ -19,7 +19,7 @@ export OPENMM_CUDA_COMPILER=`which nvcc`
 # Constants
 INSTALL_DIRECTORY="`pwd`/install"
 
-conda install -y cmake numpy scipy pytest
+conda install -y cmake numpy scipy pytest swig
 
 # Build OpenMM
 cmake -DCMAKE_INSTALL_PREFIX="${INSTALL_DIRECTORY}" -DCMAKE_CXX_COMPILER=g++ -DCMAKE_C_COMPILER=gcc .
