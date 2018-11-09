69c089df38d35051008e84359c451c47533bf9d0
Jason Swails <jason.swails@gmail.com>
2018-11-08 21:34:37 -0500

    More tweaks to the build.

--- devtools/ci/jenkins/install.sh
+++ devtools/ci/jenkins/install.sh
@@ -11,15 +11,12 @@ cmake --version
 echo "Using g++ (`which g++`) version:"
 g++ --version
 
-if [ -x `which nvcc 2>/dev/null || true` ]; then
-    echo "Using nvcc (`which nvcc`) version:"
-    nvcc --version
-    export OPENMM_CUDA_COMPILER=`which nvcc`
+if [ ! -z "$OPENMM_CUDA_COMPILER" ]; then
+    echo "Using nvcc ($OPENMM_CUDA_COMPILER) version:"
+    $OPENMM_CUDA_COMPILER --version
+    CUDA_ARGS="-DCUDA_TOOLKIT_ROOT_DIR=${CUDA_HOME}"
 fi
 
-test -z "$INSTALL_DIRECTORY" && INSTALL_DIRECTORY="`pwd`/install"
-
-# Build OpenMM
-cmake -DCMAKE_INSTALL_PREFIX="${INSTALL_DIRECTORY}" -DCMAKE_CXX_COMPILER=g++ -DCMAKE_C_COMPILER=gcc \
-      -DSWIG_EXECUTABLE=`which swig` -DCUDA_TOOLKIT_ROOT_DIR=${CUDA_HOME} $* .
+cmake -DCMAKE_INSTALL_PREFIX="`pwd`/install" -DCMAKE_CXX_COMPILER=g++ -DCMAKE_C_COMPILER=gcc \
+      -DSWIG_EXECUTABLE=`which swig` $CUDA_ARGS $* .
 make -j6 install
