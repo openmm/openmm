c9db176ef3ed15e4e79ab884e30616869b7a58bd
Jason Swails <jason.swails@gmail.com>
2018-11-26 11:20:07 -0500

    Add some debugging commands and temporarily stop testing GPU platforms.

--- devtools/ci/jenkins/install_and_test_cpu.sh
+++ devtools/ci/jenkins/install_and_test_cpu.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+whoami
+which python
+lsb_release -a
+find / -name "libdl.*"
+
 EXTRA_CMAKE_ARGS="-DOPENMM_BUILD_CUDA_LIB=false -DOPENMM_BUILD_OPENCL_LIB=false"
 . devtools/ci/jenkins/install.sh
 python devtools/run-ctest.py --job-duration=120 --timeout 300 --in-order -R 'Test(Cpu|Reference)' --parallel 4
