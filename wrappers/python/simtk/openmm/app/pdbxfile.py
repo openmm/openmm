5ef91d22429f518de9ea5c32d3b84ee54a1fbd8f
tic20 <tic20@ketch-linux.cimr.cam.ac.uk>
2019-02-19 11:04:02 +0000

    Ensuring opened files are closed properly when done

--- wrappers/python/simtk/openmm/app/pdbxfile.py
+++ wrappers/python/simtk/openmm/app/pdbxfile.py
@@ -75,6 +75,7 @@ class PDBxFile(object):
         reader = PdbxReader(inputFile)
         data = []
         reader.read(data)
+        inputfile.close()
         block = data[0]
 
         # Build the topology.
