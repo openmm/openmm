af953fadc551d586fa1758b7929e3f4ac3897831
Jason Swails <jason.swails@gmail.com>
2019-06-10 14:30:45 -0400

    Try to fix all builds in docker containers.

--- devtools/ci/jenkins/Dockerfile.opencl
+++ devtools/ci/jenkins/Dockerfile.opencl
@@ -1,4 +1,4 @@
-FROM nvidia/cuda:10.1-devel-ubuntu18.04
+FROM nvidia/opencl:devel-ubuntu18.04
 
 ENV PATH="/opt/miniconda/bin:${PATH}"
 
--- devtools/ci/jenkins/install.sh
+++ devtools/ci/jenkins/install.sh
@@ -14,7 +14,7 @@ g++ --version
 if [ ! -z "$OPENMM_CUDA_COMPILER" ]; then
     echo "Using nvcc ($OPENMM_CUDA_COMPILER) version:"
     $OPENMM_CUDA_COMPILER --version
-    CUDA_ARGS="-DCUDA_TOOLKIT_ROOT_DIR=${CUDA_HOME}"
+    CUDA_ARGS="-DCUDA_TOOLKIT_ROOT_DIR=${CUDA_HOME} -DOPENMM_BUILD_CUDA_LIB=true"
 fi
 
 cmake -DCMAKE_INSTALL_PREFIX="`pwd`/install" -DCMAKE_CXX_COMPILER=g++ -DCMAKE_C_COMPILER=gcc \
