af0e4d48d5e311f6d67a7f505a99d14e5ec04ecb
peastman <peastman@stanford.edu>
2019-07-12 10:17:59 -0700

    Updates to Vagrant VM for building 7.4

--- devtools/packaging/scripts/windows/prepare.ps1
+++ devtools/packaging/scripts/windows/prepare.ps1
@@ -2,8 +2,8 @@ cd C:\Users\vagrant
 
 # Install CUDA.
 
-wget https://developer.nvidia.com/compute/cuda/9.2/Prod2/network_installers/cuda_9.2.148_win10_network -UseBasicParsing -OutFile cuda_9.2.148_win10_network.exe
-.\cuda_9.2.148_win10_network.exe -s nvcc_9.2 cudart_9.2 cufft_9.2 cufft_dev_9.2 nvrtc_9.2 nvrtc_dev_9.2 | Out-Null
+wget https://developer.nvidia.com/compute/cuda/10.1/Prod/network_installers/cuda_10.1.168_win10_network.exe -UseBasicParsing -OutFile cuda_10.1.168_win10_network.exe
+.\cuda_10.1.168_win10_network.exe -s nvcc_10.1 cudart_10.1 cufft_10.1 cufft_dev_10.1 nvrtc_10.1 nvrtc_dev_10.1 | Out-Null
 
 # Install AMD APP SDK.
 
@@ -14,10 +14,11 @@ wget https://s3.amazonaws.com/omnia-ci/AMD-APP-SDK-v2.9-1.599.381-GA-Full-window
 
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Windows-x86_64.exe -UseBasicParsing -OutFile Miniconda3-latest-Windows-x86_64.exe
 .\Miniconda3-latest-Windows-x86_64.exe /S /D=C:\Miniconda3 | Out-Null
+[Environment]::SetEnvironmentVariable("Path", $env:Path + ";C:\Miniconda3;C:\Miniconda3\Scripts;C:\Miniconda3\Library\bin", [EnvironmentVariableTarget]::User)
 
 # Install software with conda.
 
-&﻿"C:\Miniconda3\Scripts\conda.exe" config --add channels omnia --add channels conda-forge
+& "C:\Miniconda3\Scripts\conda.exe" config --add channels omnia --add channels conda-forge
 & "C:\Miniconda3\Scripts\conda.exe" install -y fftw3f==3.3.4=vc14_2 jinja2 lxml sphinx sphinxcontrib-autodoc_doxygen sphinxcontrib-lunrsearch conda-build anaconda-client
 & "C:\Miniconda3\Scripts\pip.exe" install sphinxcontrib.bibtex
 
