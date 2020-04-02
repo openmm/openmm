cd9b1979d3347578fee12891a63fcddb0334f64e
peastman <peastman@stanford.edu>
2020-04-02 13:24:06 -0700

    Fixed exception in metadynamics

--- wrappers/python/simtk/openmm/app/metadynamics.py
+++ wrappers/python/simtk/openmm/app/metadynamics.py
@@ -6,7 +6,7 @@ Simbios, the NIH National Center for Physics-Based Simulation of
 Biological Structures at Stanford, funded under the NIH Roadmap for
 Medical Research, grant U54 GM072970. See https://simtk.org.
 
-Portions copyright (c) 2018-2019 Stanford University and the Authors.
+Portions copyright (c) 2018-2020 Stanford University and the Authors.
 Authors: Peter Eastman
 
 Permission is hereby granted, free of charge, to any person obtaining a
@@ -203,7 +203,7 @@ class Metadynamics(object):
         if len(self.variables) == 1:
             gaussian = axisGaussians[0]
         else:
-            gaussian = reduce(np.multiply.outer, reversed(axisGaussians))
+            gaussian = reduce(np.multiply.outer, axisGaussians)
 
         # Add it to the bias.
 
