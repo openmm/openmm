5e70850c2e14d0e3e92cba2dcb546014596ea161
peastman <peastman@stanford.edu>
2020-05-12 13:02:30 -0700

    bug fix for simulation.runForClockTime()

--- wrappers/python/simtk/openmm/app/simulation.py
+++ wrappers/python/simtk/openmm/app/simulation.py
@@ -195,13 +195,15 @@ class Simulation(object):
             stepsToGo = nextSteps
             while stepsToGo > 10:
                 self.integrator.step(10) # Only take 10 steps at a time, to give Python more chances to respond to a control-c.
-                stepsToGo -= 10
                 self.currentStep += 10
+                stepsToGo -= 10
                 if endTime is not None and datetime.now() >= endTime:
-                    return
-            self.integrator.step(stepsToGo)
-            self.currentStep += stepsToGo
-            if anyReport:
+                    break
+            else:
+                self.integrator.step(stepsToGo)
+                self.currentStep += stepsToGo
+                stepsToGo = 0
+            if anyReport and stepsToGo == 0:
                 # One or more reporters are ready to generate reports.  Organize them into three
                 # groups: ones that want wrapped positions, ones that want unwrapped positions,
                 # and ones that don't care about positions.
