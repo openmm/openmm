30cac992878f57e0c819938f6e5954464bd30658
peastman <peastman@stanford.edu>
2020-06-04 11:58:08 -0700

    Don't mark Drude particles as deuterium

--- wrappers/python/simtk/openmm/app/pdbfile.py
+++ wrappers/python/simtk/openmm/app/pdbfile.py
@@ -6,7 +6,7 @@ Simbios, the NIH National Center for Physics-Based Simulation of
 Biological Structures at Stanford, funded under the NIH Roadmap for
 Medical Research, grant U54 GM072970. See https://simtk.org.
 
-Portions copyright (c) 2012-2018 Stanford University and the Authors.
+Portions copyright (c) 2012-2020 Stanford University and the Authors.
 Authors: Peter Eastman
 Contributors:
 
@@ -139,8 +139,10 @@ class PDBFile(object):
                             element = elem.potassium
                         elif upper.startswith('ZN'):
                             element = elem.zinc
-                        elif( len( residue ) == 1 and upper.startswith('CA') ):
+                        elif len(residue) == 1 and upper.startswith('CA'):
                             element = elem.calcium
+                        elif upper.startswith('D') and any(a.name == atomName[1:] for a in residue.iter_atoms()):
+                            pass # A Drude particle
                         else:
                             try:
                                 element = elem.get_by_symbol(upper[0])
