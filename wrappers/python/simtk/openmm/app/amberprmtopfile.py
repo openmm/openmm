05e0c4f9d86bc793359ba126b5e7f81c793b04fb
peastman <peastman@stanford.edu>
2021-01-05 13:18:20 -0800

    Do not alter mass of hydrogens in rigid water (#2975)
    * Do not alter mass of hydrogens in rigid water
    
    * Do not alter mass of hydrogens in rigid water

--- wrappers/python/simtk/openmm/app/amberprmtopfile.py
+++ wrappers/python/simtk/openmm/app/amberprmtopfile.py
@@ -200,7 +200,8 @@ class AmberPrmtopFile(object):
         hydrogenMass : mass=None
             The mass to use for hydrogen atoms bound to heavy atoms.  Any mass
             added to a hydrogen is subtracted from the heavy atom to keep their
-            total mass the same.
+            total mass the same.  If rigidWater is used to make water molecules
+            rigid, then water hydrogens are not altered.
         ewaldErrorTolerance : float=0.0005
             The error tolerance to use if nonbondedMethod is Ewald, PME, or LJPME.
         switchDistance : float=0*nanometers
@@ -284,6 +285,8 @@ class AmberPrmtopFile(object):
             for atom1, atom2 in self.topology.bonds():
                 if atom1.element == elem.hydrogen:
                     (atom1, atom2) = (atom2, atom1)
+                if rigidWater and atom2.residue.name == 'HOH':
+                    continue
                 if atom2.element == elem.hydrogen and atom1.element not in (elem.hydrogen, None):
                     transferMass = hydrogenMass-sys.getParticleMass(atom2.index)
                     sys.setParticleMass(atom2.index, hydrogenMass)
--- wrappers/python/simtk/openmm/app/forcefield.py
+++ wrappers/python/simtk/openmm/app/forcefield.py
@@ -1135,7 +1135,8 @@ class ForceField(object):
         hydrogenMass : mass=None
             The mass to use for hydrogen atoms bound to heavy atoms.  Any mass
             added to a hydrogen is subtracted from the heavy atom to keep
-            their total mass the same.
+            their total mass the same.  If rigidWater is used to make water molecules
+            rigid, then water hydrogens are not altered.
         residueTemplates : dict=dict()
             Key: Topology Residue object
             Value: string, name of _TemplateData residue template object to use for (Key) residue.
@@ -1211,7 +1212,7 @@ class ForceField(object):
             for atom1, atom2 in topology.bonds():
                 if atom1.element is elem.hydrogen:
                     (atom1, atom2) = (atom2, atom1)
-                if atom2.element is elem.hydrogen and atom1.element not in (elem.hydrogen, None):
+                if atom2.element is elem.hydrogen and atom1.element not in (elem.hydrogen, None) and not rigidResidue[atom2.residue.index]:
                     transferMass = hydrogenMass-sys.getParticleMass(atom2.index)
                     sys.setParticleMass(atom2.index, hydrogenMass)
                     sys.setParticleMass(atom1.index, sys.getParticleMass(atom1.index)-transferMass)
