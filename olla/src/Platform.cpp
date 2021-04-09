9880d3962c906603be098bded96c2505b856ee04
wilkie <wilkie05@gmail.com>
2021-04-09 13:39:37 -0400

    Updated plugin path lookup to use OPENMM_INSTALL_PREFIX.

--- olla/src/Platform.cpp
+++ olla/src/Platform.cpp
@@ -334,7 +334,7 @@ const string& Platform::getDefaultPluginsDirectory() {
     }
 #else
     if (dir == NULL)
-        directory = "/usr/local/openmm/lib/plugins";
+        directory = OPENMM_INSTALL_PREFIX "/lib/plugins";
     else
         directory = string(dir);
 #endif
