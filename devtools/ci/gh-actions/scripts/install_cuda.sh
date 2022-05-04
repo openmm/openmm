947e557d318e1cc3f1ad3f48d22fb4cff85026bc
peastman <peastman@stanford.edu>
2022-05-04 11:21:37 -0700

    Update CUDA keys

--- devtools/ci/gh-actions/scripts/install_cuda.sh
+++ devtools/ci/gh-actions/scripts/install_cuda.sh
@@ -1,4 +1,4 @@
-# This script install CUDA on Ubuntu-based systemws
+`# This script install CUDA on Ubuntu-based systemws
 # It uses the Nvidia repos for Ubuntu 18.04, which as of Dec 2020
 # includes packages for CUDA 10.0, 10.1, 10.2, 11.0, 11.1, 11.2
 # Future versions might require an updated repo (maybe Ubuntu 20)
@@ -12,7 +12,7 @@ echo 'APT::Acquire::Retries "5";' | sudo tee /etc/apt/apt.conf.d/80-retries
 sudo wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 --tries 5 \
     -O /etc/apt/preferences.d/cuda-repository-pin-600 \
     https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
-sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/3bf863cc.pub
 sudo add-apt-repository "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/ /"
 sudo apt-get update -qq
 
