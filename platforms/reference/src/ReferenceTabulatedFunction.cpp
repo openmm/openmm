e9534c15e82e688a7e7868d2c1e10b33aeb3d556
Mike Henry <11765982+mikemhenry@users.noreply.github.com>
2022-06-22 12:09:12 -0700

    add missing header and fix logic for including cmath (#3658)

--- platforms/reference/src/ReferenceTabulatedFunction.cpp
+++ platforms/reference/src/ReferenceTabulatedFunction.cpp
@@ -33,12 +33,15 @@
     #define NOMINMAX
 #endif
 
+#include <algorithm>
+
 #include "ReferenceTabulatedFunction.h"
 #include "openmm/OpenMMException.h"
 #include "openmm/internal/SplineFitter.h"
 
-#ifdef _MSC_VER
+#include <cmath>
 
+#ifdef _MSC_VER
 #if _MSC_VER < 1800
 /**
  * We need to define this ourselves, since Visual Studio is missing round() from cmath.
@@ -46,13 +49,7 @@
 static int round(double x) {
     return (int) (x+0.5);
 }
-#else
-#include <cmath>
-#endif  // MSC_VER < 1800
-
-
-#else
-#include <cmath>
+#endif
 #endif
 
 static double wrap(double t, double min, double max) {
