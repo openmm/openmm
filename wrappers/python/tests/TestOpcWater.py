1eac346127983feb5719d6459c5c3b92879fd9e0
Alex Izvorski <alex@genesistherapeutics.ai>
2022-06-26 11:21:06 -0700

    add opc3 water test

--- wrappers/python/tests/TestOpcWater.py
+++ wrappers/python/tests/TestOpcWater.py
@@ -4,9 +4,10 @@ import openmm as mm
 from openmm import app
 from openmm import unit
 
+
 class TestOpcWater(unittest.TestCase):
 
-    def test_Energy(self):
+    def test_OpcEnergy(self):
         pdb = app.PDBFile('../openmm/app/data/opcbox.pdb')
         topology, positions = pdb.topology, pdb.positions
         self.assertEqual(len(positions), 864)
@@ -19,11 +20,7 @@ class TestOpcWater(unittest.TestCase):
             rigidWater=True,
         )
 
-        box_vectors = [
-            [1.8864844000000003, 0.       , 0.       ],
-            [0.       , 1.8478108, 0.       ],
-            [0.       , 0.       , 1.9006241],
-        ]
+        box_vectors = pdb.topology.getPeriodicBoxVectors()
         system.setDefaultPeriodicBoxVectors(*box_vectors)
         integrator = mm.LangevinIntegrator(300*unit.kelvin, 2.0/unit.picoseconds, 2.0*unit.femtoseconds)
         simulation = app.Simulation(topology, system, integrator)
@@ -31,7 +28,7 @@ class TestOpcWater(unittest.TestCase):
         context.setPositions(positions)
         context.setPeriodicBoxVectors(*box_vectors)
 
-        energy_amber = -2647.6233
+        energy_amber = -2647.6233 # kcal/mol
         energy_tolerance = 1.0
 
         state = context.getState(getEnergy=True)
@@ -46,7 +43,37 @@ class TestOpcWater(unittest.TestCase):
         # -2647.441600693312
         self.assertTrue(abs(energy1 - energy_amber) < energy_tolerance)
         self.assertTrue(abs(energy1 - energy2) < energy_tolerance)
-        
+
+
+    def test_Opc3Energy(self):
+        pdb = app.PDBFile('../openmm/app/data/opc3box.pdb')
+        topology, positions = pdb.topology, pdb.positions
+        self.assertEqual(len(positions), 648)
+        forcefield = app.ForceField('opc3.xml')
+        system = forcefield.createSystem(
+            topology,
+            nonbondedMethod=app.PME,
+            nonbondedCutoff=0.7*unit.nanometer,
+            constraints=app.HBonds,
+            rigidWater=True,
+        )
+
+        box_vectors = pdb.topology.getPeriodicBoxVectors()
+        system.setDefaultPeriodicBoxVectors(*box_vectors)
+        integrator = mm.LangevinIntegrator(300*unit.kelvin, 2.0/unit.picoseconds, 2.0*unit.femtoseconds)
+        simulation = app.Simulation(topology, system, integrator)
+        context = simulation.context
+        context.setPositions(positions)
+        context.setPeriodicBoxVectors(*box_vectors)
+
+        energy_amber = -2532.1414 # kcal/mol
+        energy_tolerance = 1.0
+
+        state = context.getState(getEnergy=True)
+        energy1 = state.getPotentialEnergy().value_in_unit(unit.kilocalorie_per_mole)
+        # -2532.4862082354407
+        self.assertTrue(abs(energy1 - energy_amber) < energy_tolerance)
+
 
 if __name__ == '__main__':
     unittest.main()
