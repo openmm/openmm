46a57c1ad16ffc2c52734b626d08b0a3c7f500a4
peastman <peastman@stanford.edu>
2022-06-27 10:24:45 -0700

    Remove FFTW as a dependency

--- devtools/packaging/scripts/osx/prepare.sh
+++ devtools/packaging/scripts/osx/prepare.sh
@@ -26,5 +26,5 @@ export PATH=$WORKSPACE/miniconda/bin:$PATH
 
 # Ensure configuration is up to date.
 conda config --add channels http://conda.binstar.org/omnia
-conda install --yes --quiet swig fftw3f pip doxygen sphinx sphinxcontrib-bibtex sphinxcontrib-lunrsearch sphinxcontrib-autodoc_doxygen lxml cmake
+conda install --yes --quiet swig pip doxygen sphinx sphinxcontrib-bibtex sphinxcontrib-lunrsearch sphinxcontrib-autodoc_doxygen lxml cmake
 pip install sphinxcontrib-bibtex sphinxcontrib-lunrsearch sphinxcontrib-autodoc_doxygen
