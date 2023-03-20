42d17b48d2a8ab3bbdfe3a974d74d74ac41e27c6
RaulPPealez <raulppelaez@gmail.com>
2023-03-20 13:52:28 +0100

    Add more tests to XTCReporter

--- wrappers/python/tests/TestXtcReporter.py
+++ wrappers/python/tests/TestXtcReporter.py
@@ -21,8 +21,8 @@ class TestXTCReporter(unittest.TestCase):
             constraints=app.HBonds,
         )
 
-    def test_WriteAll(self):
-        """Test all atoms are written"""
+    def test_OneStep(self):
+        """Test all atoms are written for a single step"""
 
         with tempfile.TemporaryDirectory() as tempdir:
             filename_pdb = os.path.join(tempdir, "temptraj.pdb")
@@ -51,6 +51,38 @@ class TestXTCReporter(unittest.TestCase):
             coords, _, _, _ = checkxtc.read()
             self.assertEqual(coords[:, :, 0].shape, validPositions.shape)
             self.assertTrue(np.allclose(coords[:, :, 0], validPositions, atol=1e-3))
+
+
+    def test_SeveralSteps(self):
+        """Test that the reporter can write several steps"""
+
+        with tempfile.TemporaryDirectory() as tempdir:
+            filename_pdb = os.path.join(tempdir, "temptraj.pdb")
+            filename_xtc = os.path.join(tempdir, "temptraj.xtc")
+            simulation = app.Simulation(
+                self.pdb.topology,
+                self.system,
+                mm.LangevinMiddleIntegrator(
+                    300 * unit.kelvin, 1.0 / unit.picosecond, 0.002 * unit.picoseconds
+                ),
+            )
+            simulation.context.setPositions(self.pdb.positions)
+
+            simulation.reporters.append(app.PDBReporter(filename_pdb, 1))
+            simulation.reporters.append(app.XTCReporter(filename_xtc, 1))
+            simulation.step(10)
+            # clear reporters to ensure PDBReporter calls writeFooter and file.close
+            simulation.reporters.clear()
+            checkxtc = app.XTCFile(filename_xtc)
+            # check the positions are correct
+            validPositions = (
+                simulation.context.getState(getPositions=True)
+                .getPositions(True)
+                .value_in_unit(unit.nanometer)
+            )
+            coords, _, _, _ = checkxtc.read()
+            self.assertEqual(coords[:, :, -1].shape, validPositions.shape)
+            self.assertTrue(np.allclose(coords[:, :, -1], validPositions, atol=1e-3))
 
 
 if __name__ == "__main__":
