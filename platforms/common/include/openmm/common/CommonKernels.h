93d5efd5aa197d72b6adb598ba4761aebd907cf4
peastman <peastman@stanford.edu>
2023-04-12 13:56:35 -0700

    Prevent molecules from accidentally getting wrapped by barostat

--- platforms/common/include/openmm/common/CommonKernels.h
+++ platforms/common/include/openmm/common/CommonKernels.h
@@ -9,7 +9,7 @@
  * Biological Structures at Stanford, funded under the NIH Roadmap for        *
  * Medical Research, grant U54 GM072970. See https://simtk.org.               *
  *                                                                            *
- * Portions copyright (c) 2008-2022 Stanford University and the Authors.      *
+ * Portions copyright (c) 2008-2023 Stanford University and the Authors.      *
  * Authors: Peter Eastman                                                     *
  * Contributors:                                                              *
  *                                                                            *
@@ -1576,6 +1576,7 @@ private:
     ComputeArray moleculeStartIndex;
     ComputeKernel kernel;
     std::vector<int> lastAtomOrder;
+    std::vector<mm_int4> lastPosCellOffsets;
 };
 
 } // namespace OpenMM
