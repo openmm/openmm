7f41337fefac180bb94f2aa7db09967aae7db83f
Peter Eastman <peastman@stanford.edu>
2023-06-21 08:24:29 -0700

    Fixed edge case in Modeller._CellList (#4120)

--- wrappers/python/openmm/app/modeller.py
+++ wrappers/python/openmm/app/modeller.py
@@ -1623,11 +1623,13 @@ class _CellList(object):
         return tuple((int(floor(pos[j]/self.cellSize[j]))%self.numCells[j] for j in range(3)))
 
     def neighbors(self, pos):
+        processedCells = set()
         offsets = (-1, 0, 1)
         for i in offsets:
             for j in offsets:
                 for k in offsets:
                     cell = self.cellForPosition(Vec3(pos[0]+i*self.cellSize[0], pos[1]+j*self.cellSize[1], pos[2]+k*self.cellSize[2]))
-                    if cell in self.cells:
+                    if cell in self.cells and cell not in processedCells:
+                        processedCells.add(cell)
                         for atom in self.cells[cell]:
                             yield atom
