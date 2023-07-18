03c97fbcae7157a2eae66c78f21848d861360eaa
Emilio Gallicchio <emilio.gallicchio@gmail.com>
2023-07-18 08:42:31 -0400

    Test ATMForce with two particles

--- tests/TestATMForce.h
+++ tests/TestATMForce.h
@@ -34,19 +34,73 @@
 #endif
 #include "openmm/internal/AssertionUtilities.h"
 #include "openmm/ATMForce.h"
+#include "openmm/CustomBondForce.h"
 #include "openmm/Context.h"
 #include "openmm/System.h"
+#include "openmm/VerletIntegrator.h"
 #include "sfmt/SFMT.h"
 #include <iostream>
 
 using namespace OpenMM;
 using namespace std;
 
+void test2Particles() {
+  // A pair of particles tethered by an harmonic bond.
+  // Displace the second one to test energy and forces at lambda=1/2
+
+  System system;
+  system.addParticle(1.0);
+  system.addParticle(1.0);
+
+  double lmbd = 0.5;
+  double lambda1 = lmbd;
+  double lambda2 = lmbd;
+  double alpha = 0.0;
+  double u0 = 0.0;
+  double w0 = 0.0;
+  double umax = 1.e6;
+  double ubcore= 0.5*1.e6;
+  double acore = 1./16.;
+  double direction = 1.0;
+
+  double displx = 1.0;
+  double disply = 0.;
+  double displz = 0.;
+
+  vector<Vec3> positions(2);
+  positions[0] = Vec3(0, 0, 0);
+  positions[1] = Vec3(0, 0, 0);
+
+  CustomBondForce* bond = new CustomBondForce("0.5*r^2");
+  bond->addBond(0, 1);
+
+  ATMForce* atm = new ATMForce(lambda1, lambda2, alpha, u0, w0, umax, ubcore, acore, direction);
+  atm->addParticle(0, 0., 0., 0.);
+  atm->addParticle(1, displx, disply, displz);
+  atm->addForce(bond);
+  system.addForce(atm);
+
+  VerletIntegrator integrator(1.0);
+  Context context(system, integrator, platform);
+  context.setPositions(positions);
+
+  State state = context.getState(State::Energy | State::Forces );
+  double epot = state.getPotentialEnergy();
+  double epert = atm->getPerturbationEnergy(context);
+
+  ASSERT_EQUAL_TOL(lmbd, context.getParameter(atm->Lambda1()), 1e-6);
+  ASSERT_EQUAL_TOL(lmbd, context.getParameter(atm->Lambda2()), 1e-6);
+  ASSERT_EQUAL_TOL(lmbd*0.5*displx*displx, epot, 1e-6);
+  ASSERT_EQUAL_TOL(0.5*displx*displx, epert, 1e-6);
+  ASSERT_EQUAL_VEC(Vec3(-lmbd*displx, 0.0, 0.0), state.getForces()[1], 1e-6);
+}
+
 void runPlatformTests();
 
 int main(int argc, char* argv[]) {
     try {
         initializeTests(argc, argv);
+        test2Particles();
         // Insert tests here
         runPlatformTests();
     }
