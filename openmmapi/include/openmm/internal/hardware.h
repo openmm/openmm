af229eb4d639a2c053e020e5865253b7bb05bfeb
ZangRuochen <657757743@qq.com>
2024-03-09 02:57:33 +0800

    Add LoongArch architecture LSX support. (#4467)
    LoongArch is a new architecture, already supported by linux-6.1, gcc-12.
    
    Signed-off-by: Zang Ruochen <zangruochen@loongson.cn>
    Co-authored-by: Zang Ruochen <zangruochen@loongson.cn>

--- openmmapi/include/openmm/internal/hardware.h
+++ openmmapi/include/openmm/internal/hardware.h
@@ -94,7 +94,7 @@ static int getNumProcessors() {
 #define cpuid __cpuid
 #else
 #if !defined(__ANDROID__) && !defined(__PNACL__) && !defined(__PPC__) \
-    && !defined(__ARM__) && !defined(__ARM64__)
+    && !defined(__ARM__) && !defined(__ARM64__) && !defined(__LOONGARCH64__)
     static void cpuid(int cpuInfo[4], int infoType) {
     #ifdef __LP64__
         __asm__ __volatile__ (
