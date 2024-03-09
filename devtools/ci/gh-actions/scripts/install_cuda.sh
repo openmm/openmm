3c01bff204b52796f83e319cf204333f5435bb5b
peastman <peastman@stanford.edu>
2024-03-08 16:40:30 -0800

    Debugging

--- devtools/ci/gh-actions/scripts/install_cuda.sh
+++ devtools/ci/gh-actions/scripts/install_cuda.sh
@@ -11,7 +11,7 @@ echo 'APT::Acquire::Retries "5";' | sudo tee /etc/apt/apt.conf.d/80-retries
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb
 sudo dpkg -i cuda-keyring_1.1-1_all.deb
 sudo apt-get update
-sudo apt-get -y install cuda=${CUDA_VERSION}.-
+sudo apt-get -y install cuda=${CUDA_VERSION}
 sudo apt-get clean
 
 export CUDA_HOME=/usr/local/cuda-${CUDA_VERSION}
