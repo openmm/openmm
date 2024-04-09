d76319315fd17e65339492a110ecb664b451e5ae
peastman <peastman@stanford.edu>
2024-04-09 14:48:09 -0700

    Fixed error when using both CustomNonbondedForce and LennardJonesForce

--- wrappers/python/openmm/app/forcefield.py
+++ wrappers/python/openmm/app/forcefield.py
@@ -6,7 +6,7 @@ Simbios, the NIH National Center for Physics-Based Simulation of
 Biological Structures at Stanford, funded under the NIH Roadmap for
 Medical Research, grant U54 GM072970. See https://simtk.org.
 
-Portions copyright (c) 2012-2022 Stanford University and the Authors.
+Portions copyright (c) 2012-2024 Stanford University and the Authors.
 Authors: Peter Eastman, Mark Friedrichs
 Contributors:
 
@@ -3010,8 +3010,9 @@ class CustomNonbondedGenerator(object):
         # Create the exclusions.
 
         bondIndices = _findBondsForExclusions(data, sys)
-        nonbonded = [f for f in sys.getForces() if isinstance(f, mm.CustomNonbondedForce)][0]
-        nonbonded.createExclusionsFromBonds(bondIndices, self.bondCutoff)
+        for f in sys.getForces():
+            if isinstance(f, mm.CustomNonbondedForce) and f.getEnergyFunction() == self.energy:
+                f.createExclusionsFromBonds(bondIndices, self.bondCutoff)
 
 parsers["CustomNonbondedForce"] = CustomNonbondedGenerator.parseElement
 
