10abd0af00a151df9308ebeb63def85045061ef5
Marc Schuh <marc.schuh@boku.ac.at>
2024-04-09 10:38:18 +0200

    added type checking for Simulation.step()

--- wrappers/python/openmm/app/simulation.py
+++ wrappers/python/openmm/app/simulation.py
@@ -142,8 +142,11 @@ class Simulation(object):
         """
         mm.LocalEnergyMinimizer.minimize(self.context, tolerance, maxIterations, reporter)
 
-    def step(self, steps):
+    def step(self, steps: int):
         """Advance the simulation by integrating a specified number of time steps."""
+        if not isinstance(steps, int):
+            raise TypeError(f'Expected type int for steps, got {type(steps)}')
+            
         self._simulate(endStep=self.currentStep+steps)
 
     def runForClockTime(self, time, checkpointFile=None, stateFile=None, checkpointInterval=None):
