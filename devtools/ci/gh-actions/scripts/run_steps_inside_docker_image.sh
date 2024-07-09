86f798c8082d336f3ebed59f7b58719d74122d71
peastman <peastman@stanford.edu>
2024-07-09 11:32:53 -0700

    Debugging

--- devtools/ci/gh-actions/scripts/run_steps_inside_docker_image.sh
+++ devtools/ci/gh-actions/scripts/run_steps_inside_docker_image.sh
@@ -9,8 +9,9 @@ echo "::endgroup::"
 echo "::group::Prepare build environment..."
 extra_conda_packages=""
 if [[ ${COMPILERS} == devtoolset* ]]; then
-    sudo sed -i.bak 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
-    sudo sed -i.bak 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
+    #sudo sed -i.bak 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
+    #sudo sed -i.bak 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
+    sudo ls
     sudo yum install -y centos-release-scl
     sudo yum install -y ${COMPILERS}
     source /opt/rh/${COMPILERS}/enable
