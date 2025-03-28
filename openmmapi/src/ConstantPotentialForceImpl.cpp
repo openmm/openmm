857babd5232c2cb0e3269f9cbcada5e2457b3410
Evan Pretti <pretti@stanford.edu>
2025-03-28 15:10:14 -0700

    Initial implementation of C++ API

---
+++ openmmapi/src/ConstantPotentialForceImpl.cpp
@@ -0,0 +1,150 @@
+/* -------------------------------------------------------------------------- *
+ *                                   OpenMM                                   *
+ * -------------------------------------------------------------------------- *
+ * This is part of the OpenMM molecular simulation toolkit originating from   *
+ * Simbios, the NIH National Center for Physics-Based Simulation of           *
+ * Biological Structures at Stanford, funded under the NIH Roadmap for        *
+ * Medical Research, grant U54 GM072970. See https://simtk.org.               *
+ *                                                                            *
+ * Portions copyright (c) 2008-2024 Stanford University and the Authors.      *
+ * Authors: Peter Eastman                                                     *
+ * Contributors:                                                              *
+ *                                                                            *
+ * Permission is hereby granted, free of charge, to any person obtaining a    *
+ * copy of this software and associated documentation files (the "Software"), *
+ * to deal in the Software without restriction, including without limitation  *
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,   *
+ * and/or sell copies of the Software, and to permit persons to whom the      *
+ * Software is furnished to do so, subject to the following conditions:       *
+ *                                                                            *
+ * The above copyright notice and this permission notice shall be included in *
+ * all copies or substantial portions of the Software.                        *
+ *                                                                            *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR *
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,   *
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL    *
+ * THE AUTHORS, CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,    *
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR      *
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE  *
+ * USE OR OTHER DEALINGS IN THE SOFTWARE.                                     *
+ * -------------------------------------------------------------------------- */
+
+#ifdef WIN32
+  #define _USE_MATH_DEFINES // Needed to get M_PI
+#endif
+#include "openmm/OpenMMException.h"
+#include "openmm/internal/ContextImpl.h"
+#include "openmm/internal/Messages.h"
+#include "openmm/internal/ConstantPotentialForceImpl.h"
+#include "openmm/kernels.h"
+#include <cmath>
+#include <map>
+#include <sstream>
+#include <algorithm>
+
+using namespace OpenMM;
+using namespace std;
+
+ConstantPotentialForceImpl::ConstantPotentialForceImpl(const ConstantPotentialForce& owner) : owner(owner) {
+    forceGroup = owner.getForceGroup();
+}
+
+ConstantPotentialForceImpl::~ConstantPotentialForceImpl() {
+}
+
+void ConstantPotentialForceImpl::initialize(ContextImpl& context) {
+    kernel = context.getPlatform().createKernel(CalcConstantPotentialForceKernel::Name(), context);
+
+
+    const System& system = context.getSystem();
+    if (owner.getNumParticles() != system.getNumParticles())
+        throw OpenMMException("ConstantPotentialForce must have exactly as many particles as the System it belongs to.");
+
+    // Check for errors in the specification of exceptions.
+    vector<set<int> > exceptions(owner.getNumParticles());
+    for (int i = 0; i < owner.getNumExceptions(); i++) {
+        int particle[2];
+        double chargeProd;
+        owner.getExceptionParameters(i, particle[0], particle[1], chargeProd);
+        int minp = min(particle[0], particle[1]);
+        int maxp = max(particle[0], particle[1]);
+        for (int j = 0; j < 2; j++) {
+            if (particle[j] < 0 || particle[j] >= owner.getNumParticles()) {
+                stringstream msg;
+                msg << "ConstantPotentialForce: Illegal particle index for an exception: ";
+                msg << particle[j];
+                throw OpenMMException(msg.str());
+            }
+        }
+        if (exceptions[minp].count(maxp) > 0) {
+            stringstream msg;
+            msg << "ConstantPotentialForce: Multiple exceptions are specified for particles ";
+            msg << particle[0];
+            msg << " and ";
+            msg << particle[1];
+            throw OpenMMException(msg.str());
+        }
+        exceptions[minp].insert(maxp);
+    }
+
+    // Check for problems with the periodic box vectors.
+    Vec3 boxVectors[3];
+    system.getDefaultPeriodicBoxVectors(boxVectors[0], boxVectors[1], boxVectors[2]);
+    double cutoff = owner.getCutoffDistance();
+    if (cutoff > 0.5*boxVectors[0][0] || cutoff > 0.5*boxVectors[1][1] || cutoff > 0.5*boxVectors[2][2])
+        throw OpenMMException("ConstantPotentialForce: "+Messages::cutoffTooLarge);
+
+    // Check for errors in the specification of electrodes.
+    vector<int> electrodes(owner.getNumParticles(), -1);
+    for (int electrode = 0; electrode < owner.getNumElectrodes(); electrode++) {
+        std::set<int> particles;
+        double potential;
+        double gaussianWidth;
+        double thomasFermiScale;
+        owner.getElectrodeParameters(electrode, particles, potential, gaussianWidth, thomasFermiScale);
+
+        for (int particle : particles) {
+            if (electrodes[particle] != -1) {
+                stringstream msg;
+                msg << "ConstantPotentialForce: Particle " << particle << " belongs to electrodes " << electrodes[particle] << " and " << electrode;
+                throw OpenMMException(msg.str());
+            }
+            electrodes[particle] = electrode;
+        }
+
+        if (gaussianWidth < 0) {
+            stringstream msg;
+            msg << "ConstantPotentialForce: Electrode " << electrode << " has negative Gaussian width";
+            throw OpenMMException(msg.str());
+        }
+        if (thomasFermiScale < 0) {
+            stringstream msg;
+            msg << "ConstantPotentialForce: Electrode " << electrode << " has negative Thomas-Fermi scale";
+            throw OpenMMException(msg.str());
+        }
+    }
+
+    kernel.getAs<CalcConstantPotentialForceKernel>().initialize(context.getSystem(), owner);
+}
+
+double ConstantPotentialForceImpl::calcForcesAndEnergy(ContextImpl& context, bool includeForces, bool includeEnergy, int groups) {
+    if ((groups & (1 << forceGroup)) != 0) {
+        return kernel.getAs<CalcConstantPotentialForceKernel>().execute(context, includeForces, includeEnergy);
+    }
+    return 0.0;
+}
+
+std::vector<std::string> ConstantPotentialForceImpl::getKernelNames() {
+    std::vector<std::string> names;
+    names.push_back(CalcConstantPotentialForceKernel::Name());
+    return names;
+}
+
+void ConstantPotentialForceImpl::updateParametersInContext(ContextImpl& context, int firstParticle, int lastParticle, int firstException, int lastException) {
+    kernel.getAs<CalcConstantPotentialForceKernel>().copyParametersToContext(context, owner, firstParticle, lastParticle, firstException, lastException);
+    context.systemChanged();
+}
+
+void ConstantPotentialForceImpl::getPMEParameters(double& alpha, int& nx, int& ny, int& nz) const {
+    kernel.getAs<CalcConstantPotentialForceKernel>().getPMEParameters(alpha, nx, ny, nz);
+}
