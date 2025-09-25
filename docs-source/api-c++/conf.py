3076825e19cca3cab3c31c0582ee66404ffd8578
Evan Pretti <pretti@stanford.edu>
2025-09-25 08:59:23 -0700

    Update logo, styles and font in documentation (#5090)

--- docs-source/api-c++/conf.py
+++ docs-source/api-c++/conf.py
@@ -37,8 +37,8 @@ html_theme_options = {
     "github_button": False,
     "github_user": "openmm",
     "github_repo": "openmm",
-    "logo_name": True,
-    "logo": "logo.png",
+    "logo_name": "OpenMM C\u2060+\u2060+ API",
+    "logo": "logo.svg",
     "extra_nav_links": [
         {
             "title": "OpenMM.org",
@@ -72,6 +72,9 @@ html_theme_options = {
         },
     ],
     "show_relbar_bottom": True,
+    "font_family": "Roboto, sans-serif",
+    "head_font_family": "Roboto, sans-serif",
+    "caption_font_family": "Roboto, sans-serif",
 }
 html_sidebars = {
     "**": [
