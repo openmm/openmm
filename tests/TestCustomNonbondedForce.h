26df7a872512ce427bb3d7be817ebf37fe99fff7
Peter Eastman <peastman@stanford.edu>
2026-03-27 08:54:42 -0700

    Cache coefficients for long range correction (#5239)
    * Cache coefficients for long range correction
    
    * updateParametersInContext() clears cache

--- tests/TestCustomNonbondedForce.h
+++ tests/TestCustomNonbondedForce.h
@@ -1498,7 +1498,7 @@ void testComputedValues(int mode) {
 
     Context context(system, integrator, platform);
     context.setPositions(positions);
-    for (double lambda : {0.0, 0.3, 0.7, 1.0}) {
+    for (double lambda : {0.0, 0.3, 0.7, 1.0, 0.3}) { // Testing 0.3 twice checks caching of long range correction coefficient
         context.setParameter("lambda", lambda);
         double e1 = context.getState(State::Energy, false, 1<<0).getPotentialEnergy();
         double e2 = context.getState(State::Energy, false, 1<<1).getPotentialEnergy();
