8566ebf6e916cae5eabc89a12dbc3ccd8d84e412
peastman <peastman@stanford.edu>
2026-04-01 13:39:25 -0700

    Reduced memory used by SerializationNode

--- serialization/tests/TestSerializationNode.cpp
+++ serialization/tests/TestSerializationNode.cpp
@@ -65,12 +65,12 @@ void testProperties() {
     ASSERT_EQUAL("abc", node.getStringProperty("prop1", "abc"));
     node.setIntProperty("prop1", 1);
     ASSERT_EQUAL(1, node.getIntProperty("prop1"));
-    node.setDoubleProperty("prop1", 1.5);
-    ASSERT_EQUAL(1.5, node.getDoubleProperty("prop1"));
-    node.setStringProperty("prop1", "hello");
-    ASSERT_EQUAL("hello", node.getStringProperty("prop1"));
+    node.setDoubleProperty("prop2", 1.5);
+    ASSERT_EQUAL(1.5, node.getDoubleProperty("prop2"));
+    node.setStringProperty("prop3", "hello");
+    ASSERT_EQUAL("hello", node.getStringProperty("prop3"));
     ASSERT_EQUAL(true, node.hasProperty("prop1"));
-    ASSERT_EQUAL(false, node.hasProperty("prop2"));
+    ASSERT_EQUAL(false, node.hasProperty("prop4"));
 }
 
 int main() {
