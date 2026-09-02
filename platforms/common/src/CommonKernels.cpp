e4e06c3ed808819d8d914fee951bbf0227e66043
Peter Eastman <peter.eastman@gmail.com>
2026-09-02 14:43:21 -0700

    Revert "Disable worker thread in PythonForce (#5412)"
    This reverts commit 0da03998df892bbb0a954ad3767c30a0cc53a11c.

--- platforms/common/src/CommonKernels.cpp
+++ platforms/common/src/CommonKernels.cpp
@@ -4867,9 +4867,7 @@ void CommonCalcPythonForceKernel::initialize(const ContextImpl& context, const P
         addForcesKernel->addArg(cc.getAtomIndexArray());
     }
     forceGroupFlag = (1<<force.getForceGroup());
-    // Disable the worker thread until we figure out what is causing incompatibilities with PyTorch.
-//    useWorkerThread = (cc.getNumContexts() == 1);
-    useWorkerThread = false;
+    useWorkerThread = (cc.getNumContexts() == 1);
     for (const ForceImpl* impl : context.getForceImpls())
         if (&impl->getOwner() != &force && (dynamic_cast<const CustomCPPForceImpl*>(impl) != NULL || dynamic_cast<const PythonForceImpl*>(impl) != NULL))
             useWorkerThread = false;
