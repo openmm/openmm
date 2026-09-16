b3857999e6f688eb4967c3fa9171a605225d6959
Lester Hedges <lester.hedges@gmail.com>
2026-09-16 09:11:37 +0100

    Reuse long range correction data across CustomNonbondedForce parameter updates.

--- platforms/reference/src/ReferenceKernels.cpp
+++ platforms/reference/src/ReferenceKernels.cpp
@@ -1592,7 +1592,7 @@ void ReferenceCalcCustomNonbondedForceKernel::copyParametersToContext(ContextImp
     
     if (useLongRangeCorrection) {
         ThreadPool& threads = extractThreadPool(context);
-        longRangeCorrectionData = CustomNonbondedForceImpl::prepareLongRangeCorrection(force, threads.getNumThreads());
+        CustomNonbondedForceImpl::updateLongRangeCorrection(force, longRangeCorrectionData, threads.getNumThreads());
         CustomNonbondedForceImpl::calcLongRangeCorrection(longRangeCorrectionData, context.getOwner(), longRangeCoefficient, longRangeCoefficientDerivs, threads);
         hasInitializedLongRangeCorrection = true;
     }
